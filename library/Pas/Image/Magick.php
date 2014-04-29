621722a1678583189eddb03efd9f6cbb6ccb9016
portableant <dpett@britishmuseum.org>
2014-04-29 12:04:11 +0100

    Refactoring

--- library/Pas/Image/Magick.php
+++ library/Pas/Image/Magick.php
@@ -13,6 +13,8 @@ class Pas_Image_Magick {
 	
 	protected $_userPath;
 	
+	protected $_original;
+	
 	protected $_extensions	= array( 'jpg', 'jpeg', 'png', 'tiff', 'tif');
 	
 	protected $_mimeTypes 	= array( 
@@ -41,7 +43,7 @@ class Pas_Image_Magick {
 	
 	const EXT		= '.jpg';
 	
-	const IMAGEDIR	= 'images/';
+	const IMAGEDIR	= './images/';
 	
 	public function __construct( )
 	{
@@ -49,13 +51,14 @@ class Pas_Image_Magick {
 			array ( 'destination' => self::THUMB, 'width' => 100, 'height' => 100 ),
 			array ( 'destination' => self::SMALL, 'width' => 100, 'height' => 100 ),
 			array ( 'destination' => self::MEDIUM, 'width' => 100, 'height' => 100 ),
-			array ( 'destination' => self::DISPLAY, 'width' => 100, 'height' => 100 )
+			array ( 'destination' => self::DISPLAY, 'width' => 100, 'height' => 100 ),
 		);
 		$this->_user = new Pas_User_Details();
 	}
 	
 	public function setOriginal(  $original ) {
-		
+		$this->_original = $original;
+		return $this;	
 	}
 
 	public function getUserPath( )
@@ -63,7 +66,36 @@ class Pas_Image_Magick {
 		$this->_userPath = $this->_user->getPerson()->username;		
 	}
 	
-	public function resize
+	public function checkDirectories()
+	{
+		foreach( $this->_sizes as $dir )
+		{
+			$directory = self::IMAGEDIR . $this->getUserPath() . $dir['destination'];
+			if(!is_dir( $directory ))
+			{
+				mkdir($directory, 755);	
+			}
+		}
+		return $this;
+	}
+	
+	public function resize( ) 
+	{
+		$path =  self::IMAGEDIR . $this->getUserPath() . $this->_original;
+		$this->checkDirectories();
+		$name = $this->_original;	
+		foreach( $this->_sizes as $resize )
+		{
+			$newImage = self::IMAGEDIR . $this->getUserPath() . $resize['destination'] . self::EXT;
+			$image = Image::create( $path, 'Imagick' );
+			$mime = $image->getMimeType();
+			if( in_array( $mime, $this->_mimeTypes ) )
+			{
+				$image->resize( $resize['width'], $resize['height'], 1);
+				$image->save($newImage);
+			}
+		}
+	}
 	
 	
 }
