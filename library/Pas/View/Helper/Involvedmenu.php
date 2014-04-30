52553bf23336306ad3b7bd144bebba386d40e99d
portableant <dpett@britishmuseum.org>
2014-04-30 15:27:12 +0100

    Changes made to view helpers and menus adapted.

--- library/Pas/View/Helper/Involvedmenu.php
+++ library/Pas/View/Helper/Involvedmenu.php
@@ -11,25 +11,49 @@
  * @since September 13 2011
  * @todo change the class to use zend_navigation
 */
-class Pas_View_Helper_Involvedmenu 
+class Pas_View_Helper_InvolvedMenu 
 	extends Zend_View_Helper_Abstract {
 
+	protected $_front, $_param;
+	
+	public function __construct()
+	{
+		$this->_front = Zend_Controller_Front::getInstance()->getRequest();
+		$this->_param = $this->_front->getParam('slug');
+	}	
+		
 	/** Build the get involved menu 
 	 * @todo convert to Zend Navigation
 	 * @return string $html The menu html 
 	 */
-	public function involvedmenu(){
-	$treasure = new Content();
-	$treasure = $treasure->buildMenu('getinvolved', $front = 0, $publish = 3);
-	$html = '';
-	foreach($treasure as $t) {
-	$html .= '<li><a href="';
-	$html .= $this->view->url(array('module' => 'getinvolved', 'controller' => 'guides', 'action' => 'index',
-	'slug' => $t['slug']), 'guides', true);
-	$html .= '" title="Read more">';
-	$html .= $t['menuTitle'];
-	$html .= '</a></li>';
+	public function involvedMenu()
+	{
+		return $this;
 	}
-	return $html;
+	
+	public function menu()
+	{
+		$treasure = new Content();
+		$treasure = $treasure->buildMenu('getinvolved', $front = 0, $publish = 3);
+		$html = '';
+		foreach($treasure as $t) {
+		$html .= '<li ';
+		if($t['slug'] == $this->_param) {
+		$html .= 'class="active"';
+		}
+		$html .= '>';
+		$html .= '<a href="';
+		$html .= $this->view->url(array('module' => 'getinvolved', 'controller' => 'guides', 'action' => 'index',
+		'slug' => $t['slug']), 'guides', true);
+		$html .= '" title="Read more">';
+		$html .= $t['menuTitle'];
+		$html .= '</a></li>';
+		}
+		return $html;
+	}
+	
+	public function __toString()
+	{
+		return $this->menu();
 	}
 }
--- library/Pas/View/Helper/TreasureReportsMenu.php
+++ library/Pas/View/Helper/TreasureReportsMenu.php
@@ -15,26 +15,49 @@
 class Pas_View_Helper_TreasureReportsMenu
 	extends Zend_View_Helper_Abstract {
 
+	protected $_front, $_param;
+	
+	public function __construct()
+	{
+		$this->_front = Zend_Controller_Front::getInstance()->getRequest();
+		$this->_param = $this->_front->getParam('slug');
+	}	
+	
 	/** Display the treasure reports menu
 	* @access public
 	* @return string $html
 	*/
-	public function treasurereportsmenu(){
-	$treasure = new Content();
-	$treasure = $treasure->buildTMenu();
-	$html = '';
-	foreach($treasure as $t) {
-	$html .= '<li class="menu collapsible nav nav-stacked nav-pills"><a href="';
-	$html .= $this->view->url(array(
-		'module' => 'treasure',
-		'controller' => 'reports',
-		'action' => 'index',
-		'slug' => $t['slug']),
-		'treps', true);
-	$html .= '" title="Read more">';
-	$html .= $t['menuTitle'];
-	$html .= '</a></li>';
+	public function treasureReportsMenu(){
+		return $this;
 	}
-	return $html;
+
+	public function menu()
+	{
+		$treasure = new Content();
+		$treasure = $treasure->buildTMenu();
+		$html = '';
+		foreach($treasure as $t) {
+		$html .= '<li class="';
+		if($t['slug'] == $this->_param) {
+		$html .= 'active';
+		}
+		$html .= ' ">';
+		$html .= '<a href="';
+		$html .= $this->view->url(array(
+			'module' => 'treasure',
+			'controller' => 'reports',
+			'action' => 'index',
+			'slug' => $t['slug']),
+			'treps', true);
+		$html .= '" title="Read more">';
+		$html .= $t['menuTitle'];
+		$html .= '</a></li>';
+		}
+		return $html;
+	}
+	
+	public function __toString()
+	{
+		return $this->menu();
 	}
 }
