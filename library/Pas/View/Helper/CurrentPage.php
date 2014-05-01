ca59aa51e6b3b4e96fdeee31b8270a60d9ac1c05
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2014-05-01 12:33:40 +0100

    New view helper to make current page link active

---
+++ library/Pas/View/Helper/CurrentPage.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * A view helper for creating active css class for current page
+ * @category   Pas
+ * @package    Pas_View_Helper
+ * @subpackage Abstract
+ * @copyright  Copyright (c) 2014 mchester-kadwell @ britishmuseum.org
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @see Zend_View_Helper_Abstract
+ */
+
+class Pas_View_Helper_CurrentPage extends Zend_View_Helper_Abstract {
+   
+    protected $_module;
+    protected $_controller;
+    protected $_action;
+  
+    /** Get the current instance of module, controller and action
+     *  
+     */    
+    public function __construct(){    
+        $front = Zend_Controller_Front::getInstance()->getRequest();
+        $this->_module = $front->getModuleName();
+        $this->_controller = $front->getControllerName();
+        $this->_action = $front->getActionName();
+    }
+
+    /** Create active css class for link if current instance matches 
+     *  
+     */
+    public function active($module, $controller, $action){
+        if($module == $this->_module && $controller == $this->_controller && $action == $this->_action) {
+            return 'class="active"';
+        }
+    }
+}
+
+/**
+* usage example
+* echo $this->active('contacts', 'index', 'index');
+* /
