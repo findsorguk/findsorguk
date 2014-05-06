216a03b852b53a70d8948f743851d5239f3ba18b
Daniel Pett <dpett@britishmuseum.org>
2014-05-06 16:28:51 +0100

    First batch of view helpers refactored.

--- library/Pas/View/Helper/Adminperiod.php
+++ library/Pas/View/Helper/Adminperiod.php
@@ -1,37 +1,85 @@
- <?php
-
-class Pas_View_Helper_Adminperiod extends Zend_View_Helper_Abstract
+<?php
+/**
+ * A view helper for automatically inserting period name from integer
+ * This function is a bit of a waste of space!
+ * @category   Pas
+ * @package    Pas_View_Helper
+ * @subpackage Abstract
+ * @copyright  Copyright (c) 2011 dpett @ britishmuseum.org
+ * @license    GNU Public
+ * @see Zend_View_Helper_Abstract
+ * @author Daniel Pett <dpett@britishmuseum.org>
+ * @copyright (c) 2014, Daniel Pett, The British Museum
+ * @uses Zend_Exception
+ * @version 2
+ * @since 1
+ *
+ */
+class Pas_View_Helper_AdminPeriod extends Zend_View_Helper_Abstract
 {
-  public function adminperiod($period)
-     {
-        if ($period == 21) {
-           $periodName =  'Roman';
+    /** The period variable
+     *
+     * @var integer
+     */
+    protected $_period;
+
+    /** Construct the function
+     *
+     * @param integer $period
+     */
+    public function __construct( $period ) {
+        if(is_int($period)){
+        $this->_period = $period;
+        } else {
+            throw new Zend_Exception('Value must be an integer');
         }
-		if ($period == 47)
-		{
-		$periodName = 'Early Medieval';
-		}
-		if ($period == 16)
-		{
-		$periodName = 'Iron Age';
-		}
-		if ($period == 29)
-		{
-		$periodName = 'Medieval';
-		}
-		if ($period == 36)
-		{
-		$periodName = 'Post Medieval';
-		}
-		if ($period == 66)
-		{
-		$periodName = 'Greek and Roman Provincial';
-		}
-		if ($period == 67)
-		{
-		$periodName = 'Byzantine';
-		}
-		return $periodName;
+    }
+
+    /** Base function for class
+     *
+     * @return \Pas_View_Helper_AdminPeriod
+     */
+    public function adminPeriod(){
+        return $this;
+    }
+
+    /** Switch function for determining the html to return
+     *
+     * @return string
+     */
+    public function html() {
+
+        switch($this->_period) {
+            case 21 :
+                $periodName = 'Roman';
+                break;
+            case 47 :
+                $periodName = 'Early Medieval';
+                break;
+            case 16 :
+                $periodName = 'Iron Age';
+                break;
+            case 29 :
+                $periodName = 'Medieval';
+                break;
+            case 36 :
+                $periodName = 'Post Medieval';
+                break;
+            case 66 :
+                $periodName = 'Greek and Roman Provincial';
+                break;
+            case 67 :
+                $periodName = 'Byzantine';
+                break;
+        }
+        return $periodName;
+     }
+
+     /** Magic method to return the html
+      *
+      * @return string
+      */
+     public function __toString() {
+         return $this->html();
      }
  }
- ?>
--- library/Pas/View/Helper/Coins.php
+++ library/Pas/View/Helper/Coins.php
@@ -11,72 +11,122 @@
  * @uses Pas_View_Helper_CurUrl
  */
 class Pas_View_Helper_Coins extends Zend_View_Helper_Abstract {
-	/**
-	 * Get the accessed date and time in correct format
-	 */
-	private function _Accessed(){
-	$access = Zend_Date::now()->toString('yyyy-MM-dd HH:mm');
-	return $access;
-	}
 
-	/** Set up the constants for the metadata
-	 *
-	 * @var unknown_type
-	 */
-	const COINS_SPAN_CLASS 						= 'Z3988';
-	const CTX_VER          						= 'Z39.88-2004';
-	const RFT_VAL_FMT      						= 'info:ofi/fmt:kev:mtx:dc';
-	const RFR_ID								= 'info:sid/finds.org.uk:generator';
-	const ELEMENT_SET_DUBLIN_CORE				= 'Dublin Core';
-	const ELEMENT_TITLE_DEFAULT					= '[unknown title]';
-	const ELEMENT_DESCRIPTION_TRUNCATE_LENGTH	= 500;
-	const ELEMENT_SUBJECT 						= 'Archaeology';
-	const ELEMENT_LANGUAGE 						= 'English';
-	const ELEMENT_RIGHTS 						= 'CC BY-SA';
-	const ELEMENT_PUBLISHER 					= 'The Portable Antiquities Scheme';
-	const ELEMENT_TYPE 							= 'WebPage';
-	const ELEMENT_FORMAT 						= 'Text';
+    protected $_access;
 
-	/** Create the coinsdata array
-	 *
-	 * @var unknown_type
-	 */
-	private $_coinsData = array();
+    protected $_coinsData = array();
 
-	/** Generate the metadata for insert
-	 *
-	 * @param string $title
-	 * @param string $author
-	 * @param date $published
-	 * @param string $description
-	 */
-	public function Coins($title = NULL, $author = NULL,$published = NULL,$description = NULL)  {
+    public $_author = 'The Portable Antiquities Scheme';
 
-	$description = substr($description, 0, self::ELEMENT_DESCRIPTION_TRUNCATE_LENGTH);
+    public $_title = 'A webpage from the Portable Antiquities Scheme';
 
-	//Create the array for the data
-	$this->_coinsData['ctx_ver']     = self::CTX_VER;
+    public $_published;
+
+    const COINS_SPAN_CLASS = 'Z3988';
+    const CTX_VER = 'Z39.88-2004';
+    const RFT_VAL_FMT = 'info:ofi/fmt:kev:mtx:dc';
+    const RFR_ID = 'info:sid/finds.org.uk:generator';
+    const ELEMENT_SET_DUBLIN_CORE = 'Dublin Core';
+    const ELEMENT_TITLE_DEFAULT	= '[unknown title]';
+    const ELEMENT_DESCRIPTION_TRUNCATE_LENGTH	= 500;
+    const ELEMENT_SUBJECT  = 'Archaeology';
+    const ELEMENT_LANGUAGE  = 'English';
+    const ELEMENT_RIGHTS  = 'CC BY-SA';
+    const ELEMENT_PUBLISHER = 'The Portable Antiquities Scheme';
+    const ELEMENT_TYPE = 'WebPage';
+    const ELEMENT_FORMAT = 'Text';
+
+    /** Get the accessed date
+     *
+     * @return \Pas_View_Helper_Coins
+     */
+
+    public function getAccessed(){
+        $this->_access = Zend_Date::now()->toString('yyyy-MM-dd HH:mm');
+        $this->_published = $this->_access;
+        return $this;
+    }
+
+    /** Set the title for the coins object
+     *
+     * @param string $title
+     * @return \Pas_View_Helper_Coins
+     */
+    public function setTitle( $title ) {
+        $this->_title = $title;
+        return $this;
+    }
+
+    /** Set the author
+     *
+     * @param string $author
+     * @return \Pas_View_Helper_Coins
+     */
+    public function setAuthor( $author ){
+        $this->_author = $author;
+        return $this;
+    }
+
+    /** Set the date published
+     *
+     * @param type $published
+     * @return \Pas_View_Helper_Coins
+     */
+    public function setPublished( $published ) {
+        $this->_published = $published;
+        return $this;
+    }
+
+    /** Set the description
+     *
+     * @param type $description
+     * @return \Pas_View_Helper_Coins
+     */
+    public function setDescription( $description ){
+        $this->_description = substr($description, 0,
+                self::ELEMENT_DESCRIPTION_TRUNCATE_LENGTH);
+        return $this;
+    }
+
+    /** The coins object
+     *
+     * @return \Pas_View_Helper_Coins
+     */
+    public function coins() {
+        return $this;
+    }
+
+    public function __toString() {
+        return $this->html();
+    }
+
+    /** return the html after using the array of data
+     *
+     * @return string
+     */
+    public function html() {
+        $this->_coinsData['ctx_ver']     = self::CTX_VER;
 	$this->_coinsData['rft_val_fmt'] = self::RFT_VAL_FMT;
 	$this->_coinsData['rfr_id']      = self::RFR_ID;
-	$this->_coinsData['rft.title']  = $title . ' - Portable Antiquities Scheme';
+	$this->_coinsData['rft.title']  = $this->view->title($this->view->headTitle());
 	$this->_coinsData['rft.subject'] = self::ELEMENT_SUBJECT;
 	$this->_coinsData['rft.language'] = self::ELEMENT_LANGUAGE;
 	$this->_coinsData['rft.publisher'] = self::ELEMENT_PUBLISHER;
-	$this->_coinsData['rft.creator'] = $author;
-	$this->_coinsData['rft.creator'] = $author;
+	$this->_coinsData['rft.creator'] = $this->_author;
+	$this->_coinsData['rft.creator'] = $this->_author;
 	$this->_coinsData['rft.rights'] = self::ELEMENT_RIGHTS;
-	$this->_coinsData['rft.accessed'] = $this->_Accessed();
+	$this->_coinsData['rft.accessed'] = $this->_accessed();
 	$this->_coinsData['rft.type'] = self::ELEMENT_TYPE;
 	$this->_coinsData['rft.format'] = self::ELEMENT_FORMAT;
-	$this->_coinsData['rft.date'] = $published;
-	$this->_coinsData['rft.identifier'] = $this->view->CurUrl();
-	$this->_coinsData['rft.description'] = $description;
+	$this->_coinsData['rft.date'] = $this->_published;
+	$this->_coinsData['rft.identifier'] = $this->view->curUrl();
+	$this->_coinsData['rft.description'] = $this->_description;
 	//Return the HTML span by building the html query
-	$coinsSpan = '<span class="';
-    $coinsSpan .= self::COINS_SPAN_CLASS;
-    $coinsSpan .= '" title="';
-    $coinsSpan .= http_build_query($this->_coinsData, '','&amp;');
-	$coinsSpan .= '"></span>';
-	return $coinsSpan;
-	}
+	$html = '<span class="';
+        $html .= self::COINS_SPAN_CLASS;
+        $html .= '" title="';
+        $html .= http_build_query($this->_coinsData, '','&amp;');
+	$html .= '"></span>';
+	return $html;
+    }
 }
