6c98d50faf859c026dfdc0a7ee81ae55c8155891
Daniel Pett <dpett@britishmuseum.org>
2014-05-07 19:58:34 +0100

    More helper refactoring

--- library/Pas/View/Helper/IpAddress.php
+++ library/Pas/View/Helper/IpAddress.php
@@ -7,14 +7,35 @@
  * @copyright  Copyright (c) 2011 dpett @ britishmuseum.org
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @see Zend_View_Helper_Abstract
+ * @author Daniel Pett <dpett@britishmuseum.org>
  */
-class Pas_View_Helper_IpAddress extends Zend_View_Helper_Abstract {
-	
-	/**
- 	* Check for the certainty
- 	* @param $int The certainty lookup number 
- 	*/
-	public function ipAddress(){
-	return Zend_Controller_Front::getInstance()->getRequest()->getClientIp();
-	}
+class Pas_View_Helper_IpAddress extends Zend_View_Helper_Abstract
+{
+
+    protected $_ip;
+
+    /** Get the IP address from the controller
+     *
+     * @return string
+     */
+    public function getIp() {
+        $this->_ip = Zend_Controller_Front::getInstance()->getRequest()->getClientIp();
+        return $this->_ip;
+    }
+
+    /** the function
+     *
+     * @return \Pas_View_Helper_IpAddress
+     */
+    public function ipAddress() {
+        return $this;
+    }
+
+    /** The magic method
+     *
+     * @return string
+     */
+    public function __toString() {
+        return $this->getIp();
+    }
 }
--- library/Pas/View/Helper/UserAgent.php
+++ library/Pas/View/Helper/UserAgent.php
@@ -8,14 +8,35 @@
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @see Zend_View_Helper_Abstract
  */
-class Pas_View_Helper_UserAgent extends Zend_View_Helper_Abstract {
-	
-	/**
- 	* Check for the certainty
- 	* @param $int The certainty lookup number 
- 	*/
-	public function userAgent(){
-		$useragent = new Zend_Http_UserAgent();
-		return $useragent->getUserAgent();
-	}
+class Pas_View_Helper_UserAgent extends Zend_View_Helper_Abstract
+{
+
+    protected $_userAgent;
+
+    /** Get the user agent
+     *
+     * @return string
+     */
+    public function getUserAgent() {
+        $useragent = new Zend_Http_UserAgent();
+        $this->_userAgent = $useragent->getUserAgent();
+        return $this->_userAgent;
+    }
+
+    /** The function
+     *
+     * @return \Pas_View_Helper_UserAgent
+     */
+    public function userAgent() {
+        return $this;
+    }
+
+    /** Magic to string method
+     *
+     * @return string
+     */
+    public function __toString() {
+        return $this->getUserAgent();
+    }
+
 }
