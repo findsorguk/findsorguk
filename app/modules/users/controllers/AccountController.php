7ff3a706209a7bf79e7a8a4403710f94b353e8ac
Daniel Pett <dpett@britishmuseum.org>
2014-05-13 13:48:58 +0100

    Streamline the redirect

--- app/modules/users/controllers/AccountController.php
+++ app/modules/users/controllers/AccountController.php
@@ -226,8 +226,7 @@ class Users_AccountController extends Pas_Controller_Action_Admin {
 	$authAdapter = $form->username->getValidator('Authorise')->getAuthAdapter();
 	$data = $authAdapter->getResultRowObject(NULL,'password');
 	$this->_auth->getStorage()->write($data);
-	$redirect = $this->_helper->loginRedirects();
-	$this->_redirect($redirect);
+	$this->_redirect( $this->_helper->loginRedirect() );
 	} else {
 	$this->_auth->clearIdentity();
 	$this->_flashMessenger->addMessage('Sorry, there was a problem with your submission. 
