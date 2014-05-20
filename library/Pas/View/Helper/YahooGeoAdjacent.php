a0ad56d645f2097b7ce9e4fffb50133ad181222e
Daniel Pett <dpett@britishmuseum.org>
2014-05-20 16:08:18 +0100

    Amended YahooGeo helpers

--- library/Pas/View/Helper/YahooGeoAdjacent.php
+++ library/Pas/View/Helper/YahooGeoAdjacent.php
@@ -10,35 +10,82 @@
  * @since 30 September 2011
  * @uses Zend_View_Helper_Url
  */ 
-class Pas_View_Helper_YahooGeoAdjacent 
-	extends Zend_View_Helper_Abstract {
+class Pas_View_Helper_YahooGeoAdjacent extends Zend_View_Helper_Abstract 
+{
+    /** The woeid to query
+     * @access protected
+     * @var int
+     */
+    protected $_woeid;
+    
+    /** Get the woeid to query
+     * @access public
+     * @return int
+     */
+    public function getWoeid() {
+        return $this->_woeid;
+    }
+    
+    /** Set the woeid to query
+     * @access public
+     * @return int
+     */
+    public function setWoeid( int $woeid) {
+        $this->_woeid = $woeid;
+        return $this;
+    }
+
+    /** Get the places from the model
+     * @access public
+     * @return array
+     */
+    public function getPlaces() {
+        $adjacent = new GeoPlaces();
+	return $adjacent->getAdjacent($this->getWoeid());
+    }
+    
+    /** The function to return
+     * @access public
+     * @return \Pas_View_Helper_YahooGeoAdjacent
+     */
+    public function yahooGeoAdjacent() {
+        return $this;
+    }
+    
+    /** Build the html
+     * @access public
+     * @return string
+     */
+    public function buildHtml(){
+        $html = '';
+        $places = $this->getPlaces();
+        if(count($places) && !is_null($places[0]['Name'])){
+            $html .= '<h3>Adjacent places</h3>';
+            $html .= '<ul>';
+            foreach($places as $p ) {
+                $url = $this->view->url(array(
+                    'module' => 'database',
+                    'controller' => 'search',
+                    'action' => 'results',
+                    'woeid' => $p['WOE_ID']),
+                        null, true);
 	
-	/** Call database model and query for adjacencies
-	 * @param int $woeid The WOEID to query
-	 */
-	public function YahooGeoAdjacent($woeid) {
-		
-	$adjacent = new GeoPlaces();
-	$places = $adjacent->getAdjacent($woeid);
-	if(count($places) && !is_null($places[0]['Name'])){
-	$html = '<h3>Adjacent places</h3>';
-	$html .= '<ul>';
-	foreach($places as $p ) {
-	$url = $this->view->url(array(
-            'module' => 'database',
-            'controller' => 'search',
-            'action' => 'results',
-            'woeid' => $p['WOE_ID']),
-                null, true);
-	$html .= '<li><a href="' . $url . '" title="Find all objects associated 
-            with this WOEID">';
-	$html .= $p['Name'];
-	$html .= '</a></li>';
+                $html .= '<li><a href="';
+                $html .=  $url;
+                $html .= '" title="Find all objects associated with this WOEID">';
+                $html .= $p['Name'];
+                $html .= '</a></li>';
+            }
+            $html .= '</ul>';
 	}
-	$html .= '</ul>';
-	
-	return $html;
-	}
-	}
-	
+        return $html;
+    }
+    
+    /** return the string
+     * @access public
+     * @return string
+     */
+    public function __toString() {
+        return $this->buildHtml();
+    }
 }
