aabb74baa712b2e9e5f74f5cf60086cad051f3d2
Mary Chester-Kadwell <mchesterkadwell@users.noreply.github.com>
2014-06-05 17:01:35 +0100

    Tiny changes

--- app/modules/publications/controllers/IndexController.php
+++ app/modules/publications/controllers/IndexController.php
@@ -23,27 +23,6 @@ class Publications_IndexController extends Pas_Controller_Action_Admin {
     public function indexAction() {
     $content = new Content();
     $this->view->contents = $content->getFrontContent('publications');
-    $service = Zend_Gdata_Docs::AUTH_SERVICE_NAME;
-    $client = Zend_Gdata_ClientLogin::getHttpClient($this->_helper->Config()->webservice->google->username, 
-    $this->_helper->Config()->webservice->google->password, $service);
-    $docs = new Zend_Gdata_Docs($client);
-    $feed = $docs->getDocumentListFeed();
-    $documents = array();	
-    foreach ($feed->entries as $entry) {
-    $title = $entry->title;
-    foreach ($entry->link as $link) {
-        if ($link->getRel() === 'alternate') {
-           $altlink = $link->getHref();
-        }
-    }
-    $documents[]=array('title' => $title, 
-    'altlink' => $altlink,
-    'updated' => $entry->updated,
-    'type' => $entry->content->type,
-    'published' => $entry->published
-    );    
-    }
-    $this->view->documents = $documents;
     }
 
     /** Handle the requests for publications
