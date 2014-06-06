e7643e791af4eadbe84c019e3a6e55ced3ed66b6
Daniel Pett <dpett@britishmuseum.org>
2014-06-06 16:53:45 +0100

    Two more view helpers fixed

--- library/Pas/View/Helper/OnlineAccountHtml.php
+++ library/Pas/View/Helper/OnlineAccountHtml.php
@@ -1,7 +1,14 @@
 <?php
 /**
  * A view helper for displaying the online accounts in html format
- *
+ * 
+ * Example of use:
+ * <code>
+ * <?php
+ * echo $this->onlineAccountHtml()->setId($id);
+ * ?>
+ * </code>
+ * @author Daniel Pett <dpett at britishmuseum.org>
  * @category   Pas
  * @package    Pas_View_Helper
  * @subpackage Abstract
@@ -9,20 +16,56 @@
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @see Zend_View_Helper_Abstract
  */
+class Pas_View_Helper_OnlineAccountHtml extends Zend_View_Helper_Abstract {
+    
+    /** The id number to query
+     * @access public
+     * @var int
+     */
+    protected $_id;
+    
+    /** Get the ID number
+     * @access public
+     * @return int
+     */
+    public function getId() {
+        return $this->_id;
+    }
 
-class Pas_View_Helper_OnlineAccountHtml extends Zend_View_Helper_Abstract
-{
+    /** Set the ID number
+     * @access public
+     * @param int $id
+     * @return \Pas_View_Helper_OnlineAccountHtml
+     */
+    public function setId($id) {
+        $this->_id = $id;
+        return $this;
+    }
+    
     /** Retrieve a person's online accounts
      * @access public
-     * @param integer $id
+     * @return \Pas_View_Helper_OnlineAccountHtml
      */
-    public function OnlineAccountHtml($id)
-    {
-    $accts = new OnlineAccounts();
-    $data = $accts->getAccounts($id);
-    if (count($data)) {
-    return $this->buildHtml($data);
+    public function OnlineAccountHtml(){
+        return $this;
     }
+    
+    /** Get the data from the model
+     * @access public
+     * @param int $id
+     * @return array
+     */
+    public function getData( $id ){
+        $accts = new OnlineAccounts();
+        return $accts->getAccounts($id);
+    }
+    
+    /** To string function
+     * @access public
+     * @return string
+     */
+    public function __toString() {
+        return $this->buildHtml($this->getData($this->getId()));
     }
 
     /** Build HTML response
@@ -30,14 +73,14 @@ class Pas_View_Helper_OnlineAccountHtml extends Zend_View_Helper_Abstract
      * @param  array  $data
      * @return string $html
      */
-    public function buildHtml($data)
-    {
-    $html ='';
-    $html .= '<p>Social profiles: ';
-    $html .=  $this->view->partialLoop('partials/contacts/foafAccts.phtml',$data);
-    $html .= '</p>';
-
-    return  $html;
+    public function buildHtml($data) {
+        $html ='';
+        if($data) {
+            $html .= '<p>Social profiles: ';
+            $html .=  $this->view->partialLoop('partials/contacts/foafAccts.phtml',$data);
+            $html .= '</p>';
+        }
+        return  $html;
     }
 
 }
--- library/Pas/View/Helper/UserRecords.php
+++ library/Pas/View/Helper/UserRecords.php
@@ -1,6 +1,16 @@
 <?php
 /**
  * A view helper for displaying a user's record counts
+ * 
+ * An example of use:
+ * 
+ * <code>
+ * <?php
+ * echo $this->userRecords()->setUsername($username);
+ * ?>
+ * </code>
+ * 
+ * @author Daniel Pett <dpett at britishmuseum.org>
  * @category   Pas
  * @package    Pas_View_Helper
  * @subpackage Abstract
@@ -8,26 +18,73 @@
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @see Zend_View_Helper_Abstract
  */
-class Pas_View_Helper_UserRecords extends Zend_View_Helper_Abstract
-{
-    /** Display and render a user's record numbers
-     *
-     * @param string $username
+class Pas_View_Helper_UserRecords extends Zend_View_Helper_Abstract {
+   
+    /** The username
+     * @access protected
+     * @var string
      */
-    public function UserRecords($username)
-    {
-    $users = new Users();
-    $ids = $users->getUserID($username);
-    $totals = $users->getCountFinds($ids['0']['id']);
-    if ($totals['0']['finds'] > 0) {
-    $html =  '<div class="object">';
-    $html .= $totals['0']['finds'];
-    $html .= ' finds within ';
-    $html .= $totals['0']['records'];
-    $html .= ' records.</div>';
+    protected $_username;
+    
+    /** Get the username
+     * @access public
+     * @return string
+     */
+    public function getUsername() {
+        return $this->_username;
+    }
 
+    /** Set the username
+     * @access public
+     * @param string $username
+     * @return \Pas_View_Helper_UserRecords
+     */
+    public function setUsername($username) {
+        $this->_username = $username;
+        return $this;
+    }
+
+    /** The function to return
+     * @access public
+     * @return \Pas_View_Helper_UserRecords
+     */
+    public function userRecords(){
+        return $this;
+    }    
+    
+    /** The to string function
+     * @access public
+     * @return string
+     */
+    public function __toString() {
+        return $this->buildHtml($this->getData($this->getUsername()));
+    }
+    
+    /** Build the html
+     *  @access public
+     * @param array $totals
+     * @return string
+     */
+    public function buildHtml( array $totals ) {
+        $html = '';
+        if ($totals['0']['finds'] > 0) {
+            $html .=  '<div class="object">';
+            $html .= $totals['0']['finds'];
+            $html .= ' finds within ';
+            $html .= $totals['0']['records'];
+            $html .= ' records.</div>';
+        }
     return $html;
     }
+    
+    /** Get the totals for a user
+     * @access public
+     * @param string $username
+     * @return array
+     */
+    public function getData( $username ) {
+        $users = new Users();
+        $ids = $users->getUserID($username);
+        return $users->getCountFinds($ids['0']['id']);
     }
-
 }
