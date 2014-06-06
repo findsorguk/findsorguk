939bb71166c366e5b7e82f879b973c6a0033045a
Mary Chester-Kadwell <mchesterkadwell@users.noreply.github.com>
2014-06-06 16:50:26 +0100

    Tech module now functioning.

--- app/modules/guides/controllers/IndexController.php
+++ app/modules/guides/controllers/IndexController.php
@@ -1,8 +1,24 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/** Controller for index of Guides section
+ *
+ * @category   Pas
+ * @package    Pas_Controller
+ * @subpackage ActionAdmin
+ * @copyright  Copyright (c) 2011 DEJ Pett dpett @ britishmuseum . org
+ * @license    GNU General Public License
  */
+class Guides_IndexController extends Pas_Controller_Action_Admin {
 
+    /** Setup the contexts by action and the ACL.
+     */
+    public function init(){
+        $this->_helper->acl->allow('public', null);
+    }
+    /** Display the front page material.
+     */
+    public function indexAction(){
+        $content = new Content();
+        $this->view->contents = $content->getFrontContent('guides');
+    }
+
+}
