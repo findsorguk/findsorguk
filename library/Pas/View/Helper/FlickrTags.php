f78e8be7f97972283eb51fc0d3a64deec6c83357
Daniel Pett <dpett@britishmuseum.org>
2014-06-16 17:08:28 +0100

    Removed unwanted

--- library/Pas/View/Helper/FlickrTags.php
+++ library/Pas/View/Helper/FlickrTags.php
@@ -1,91 +1,125 @@
 <?php
-/** A view helper for displaying flickr tags
+/** 
+ * A view helper for displaying flickr tags
+ * 
+ * An example of use:
+ * 
+ * <code>
+ * <?php
+ * echo $this->flickrTags()
+ * ?>
+ * </code>
+ * 
  * @version 1
  * @since 7 October 2011
  * @copyright Daniel Pett
- * @author Daniel Pett
  * @category Pas
  * @package Pas_View_Helper
  * @subpackage Abstract
  * @uses Pas_Yql_Oauth
+ * @author Daniel Pett <dpett at britishmuseum.org>
  */
-class Pas_View_Helper_FlickrTags
-    extends Zend_View_Helper_Abstract {
+class Pas_View_Helper_FlickrTags extends Zend_View_Helper_Abstract {
 
+    /** The cache
+     * @access protected
+     * @var type 
+     */
     protected $_cache;
 
-    protected $_flickr;
-
-    public function __construct()
-    {
-    $this->_cache = Zend_Registry::get('cache');
-    }
-
-    /** Get flickr data
-     * @param object $flickr
+    /** The api key to access flickr
+     * @access protected
+     * @var type 
      */
-    protected function getFlickr($flickr)
-    {
-    if (!($this->_cache->test('cloud'))) {
-    $data = $this->_flickr->getTagsListUserPopular($flickr->userid);
-    $this->_cache->save($data);
-    } else {
-    $data = $this->_cache->load('cloud');
-    }
-    $tags = array();
-    foreach ($data->tag as $s) {
-    $tags[] = array('tag' => $s->content,'count' => (int) $s->count);
+    protected $_apiKey;
+    
+    protected $_api;
+    
+    public function getApi() {
+        $this->_api = new Pas_Yql_Flickr($this->getApiKey());
+        return $this->_api;
     }
 
-    return $this->createTagCloud($tags);
+        
+    public function getCache() {
+        $this->_cache = Zend_Registry::get('cache');
+        return $this->_cache;
+    }
+
+    public function getApiKey() {
+        return $this->_apiKey;
+    }
+
+    public function setCache($cache) {
+        $this->_cache = $cache;
+        return $this;
+    }
+
+    public function setApiKey($apiKey) {
+        $this->_apiKey = $apiKey;
+        return $this;
+    }
+    
+    protected function getData($flickr)  {
+        if (!($this->getCache()->test('cloud'))) {
+            $data = $this->getApi()->getTagsListUserPopular($flickr->userid);
+            $this->getCache()->save($data);
+        } else {
+            $data = $this->getCache()->load('cloud');
+        }
+        $tags = array();
+        foreach ($data->tag as $tag) {
+            $tags[] = array(
+                'tag' => $tag->content,
+                'count' => (int) $tag->count
+                    );
+        }
+        return $this->createTagCloud($tags);
     }
 
     /** Create the html array for redisplay as a tag cloud
      * @param  array  $tags
      * @return string $html
      */
-    public function createTagCloud($tags)
-    {
-    $tag = array();
-    foreach ($tags as $tagged) {
-    $tag[] = array(
-    'title' => strtolower($tagged['tag']),
-    'weight' => $tagged['count'],
-    'params' => array(
-    'url' => $this->view->url(array(
-        'module' => 'flickr',
-        'controller' => 'photos',
-        'action' => 'tagged',
-        'as' => strtolower($tagged['tag'])),
-        'default',
-        true)));
-    }
-    $tags = array(
-    'tags' => $tag,
-    'cloudDecorator' => array(
-    'decorator' => 'HtmlCloud',
-    'options' => array('htmlTags' => array(
-    'ul' => array('id' => 'period-object-cloud')))),
-    'tagDecorator' => array(
-    'decorator' => 'HtmlTag',
-    'options' => array(
-    'htmlTags' => array( 'li'),
-    'minFontSize' => 100,
-    'maxFontSize' => 200,
-    'fontSizeUnit' => '%')));
-    $cloud = new Zend_Tag_Cloud($tags);
-
-    return $cloud;
+    public function createTagCloud( array $tags) {
+        $tag = array();
+        foreach ($tags as $tagged) {
+            $tag[] = array(
+                'title' => strtolower($tagged['tag']),
+                'weight' => $tagged['count'],
+                'params' => array(
+                    'url' => $this->view->url(array(
+                        'module' => 'flickr',
+                        'controller' => 'photos',
+                        'action' => 'tagged',
+                        'as' => strtolower($tagged['tag'])),
+                            'default',
+                            true)));
+            }
+            $tags = array(
+                'tags' => $tag,
+                'cloudDecorator' => array(
+                    'decorator' => 'HtmlCloud',
+                    'options' => array('htmlTags' => array(
+                        'ul' => array('id' => 'period-object-cloud')))),
+                'tagDecorator' => array(
+                    'decorator' => 'HtmlTag',
+                    'options' => array(
+                        'htmlTags' => array('li'),
+                        'minFontSize' => 100,
+                        'maxFontSize' => 200,
+                        'fontSizeUnit' => '%')));
+        $cloud = new Zend_Tag_Cloud($tags);
+        return $cloud;
     }
 
-    /** Get tags from flickr
-     * @param object $flickr
-     */
-    public function flickrTags($flickr)
-    {
-    $this->_flickr = new Pas_Yql_Flickr($flickr);
+    public function flickrTags() {
+        return $this;
+    }
 
-    return $this->getFlickr($flickr);
+    public function __toString() {
+        $this->_flickr = new Pas_Yql_Flickr($flickr);
+        return $this->getFlickr($flickr);
     }
 
 }
