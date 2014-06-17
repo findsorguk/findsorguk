ed5e6257457eb0e352e2aca3e7b10bced8336412
Daniel Pett <dpett@britishmuseum.org>
2014-06-17 08:01:54 +0100

    Amended file name and added correct code

--- library/Pas/View/Helper/FlickrTags.php
+++ library/Pas/View/Helper/FlickrTags.php
@@ -1,20 +1,5 @@
 <?php
-<<<<<<< HEAD
-/** 
- * A view helper for displaying flickr tags
- * 
- * An example of use:
- * 
- * <code>
- * <?php
- * echo $this->flickrTags()
- * ?>
- * </code>
- * 
- * @version 1
- * @since 7 October 2011
- * @copyright Daniel Pett
-=======
+
 /**
  *  A view helper for displaying flickr tags
  *
@@ -30,71 +15,9 @@
  * @since 7 October 2011
  * @copyright Daniel Pett
  * @author Daniel Pett <dpett@britishmuseum.org>
->>>>>>> origin/issue62RefactorViewHelpers
  * @category Pas
  * @package Pas_View_Helper
  * @subpackage Abstract
- * @uses Pas_Yql_Oauth
-<<<<<<< HEAD
- * @author Daniel Pett <dpett at britishmuseum.org>
- */
-class Pas_View_Helper_FlickrTags extends Zend_View_Helper_Abstract {
-
-    /** The cache
-     * @access protected
-     * @var type 
-     */
-    protected $_cache;
-
-    /** The api key to access flickr
-     * @access protected
-     * @var type 
-     */
-    protected $_apiKey;
-    
-    protected $_api;
-    
-    public function getApi() {
-        $this->_api = new Pas_Yql_Flickr($this->getApiKey());
-        return $this->_api;
-    }
-
-        
-    public function getCache() {
-        $this->_cache = Zend_Registry::get('cache');
-        return $this->_cache;
-    }
-
-    public function getApiKey() {
-        return $this->_apiKey;
-    }
-
-    public function setCache($cache) {
-        $this->_cache = $cache;
-        return $this;
-    }
-
-    public function setApiKey($apiKey) {
-        $this->_apiKey = $apiKey;
-        return $this;
-    }
-    
-    protected function getData($flickr)  {
-        if (!($this->getCache()->test('cloud'))) {
-            $data = $this->getApi()->getTagsListUserPopular($flickr->userid);
-            $this->getCache()->save($data);
-        } else {
-            $data = $this->getCache()->load('cloud');
-        }
-        $tags = array();
-        foreach ($data->tag as $tag) {
-            $tags[] = array(
-                'tag' => $tag->content,
-                'count' => (int) $tag->count
-                    );
-        }
-        return $this->createTagCloud($tags);
-=======
  * @example /app/modules/flickr/views/scripts/index/index.phtml
  * @license http://URL name
  */
@@ -145,54 +68,12 @@ class Pas_View_Helper_FlickrTags extends Zend_View_Helper_Abstract {
      */
     public function flickrTags() {
         return $this;
->>>>>>> origin/issue62RefactorViewHelpers
     }
 
     /** To string function
      * @access public
      * @return string
      */
-<<<<<<< HEAD
-    public function createTagCloud( array $tags) {
-        $tag = array();
-        foreach ($tags as $tagged) {
-            $tag[] = array(
-                'title' => strtolower($tagged['tag']),
-                'weight' => $tagged['count'],
-                'params' => array(
-                    'url' => $this->view->url(array(
-                        'module' => 'flickr',
-                        'controller' => 'photos',
-                        'action' => 'tagged',
-                        'as' => strtolower($tagged['tag'])),
-                            'default',
-                            true)));
-            }
-            $tags = array(
-                'tags' => $tag,
-                'cloudDecorator' => array(
-                    'decorator' => 'HtmlCloud',
-                    'options' => array('htmlTags' => array(
-                        'ul' => array('id' => 'period-object-cloud')))),
-                'tagDecorator' => array(
-                    'decorator' => 'HtmlTag',
-                    'options' => array(
-                        'htmlTags' => array('li'),
-                        'minFontSize' => 100,
-                        'maxFontSize' => 200,
-                        'fontSizeUnit' => '%')));
-        $cloud = new Zend_Tag_Cloud($tags);
-        return $cloud;
-    }
-
-    public function flickrTags() {
-        return $this;
-    }
-
-    public function __toString() {
-        $this->_flickr = new Pas_Yql_Flickr($flickr);
-        return $this->getFlickr($flickr);
-=======
     public function __toString() {
         return $this->getFlickr($this->getApi());
     }
@@ -260,6 +141,5 @@ class Pas_View_Helper_FlickrTags extends Zend_View_Helper_Abstract {
                     'fontSizeUnit' => '%')));
         $cloud = new Zend_Tag_Cloud($tags);
         return $cloud;
->>>>>>> origin/issue62RefactorViewHelpers
     }
 }
