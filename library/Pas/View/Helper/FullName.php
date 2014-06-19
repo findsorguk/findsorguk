bff624ea5cd598908af1b837619b8863e60b67ad
Daniel Pett <dpett@britishmuseum.org>
2014-06-19 11:23:58 +0100

    Fix some view helper tomfoolery

--- library/Pas/View/Helper/FullName.php
+++ library/Pas/View/Helper/FullName.php
@@ -1,20 +1,18 @@
-
-<!-- saved from url=(0133)https://raw.githubusercontent.com/findsorguk/findsorguk/0e044981b8121339c5dc61d835be3d3e93683759/library/Pas/View/Helper/FullName.php -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body about="https://raw.githubusercontent.com/findsorguk/findsorguk/0e044981b8121339c5dc61d835be3d3e93683759/library/Pas/View/Helper/FullName.php"><pre style="word-wrap: break-word; white-space: pre-wrap;">&lt;?php
+<?php
 /** A view helper for getting fullname of user
  *
  * An example of use:
  *
- * &lt;code&gt;
- * &lt;?php
- * echo $this-&gt;fullName();
- * ?&gt;
- * &lt;/code&gt;
+ * <code>
+ * <?php
+ * echo $this->fullName();
+ * ?>
+ * </code>
  *
- * @author Daniel Pett &lt;dpett@britishmuseum.org&gt;
+ * @author Daniel Pett <dpett@britishmuseum.org>
  * @version 1
  * @since 1
- * @copyright Daniel Pett &lt;dpett@britishmuseum.org&gt;
+ * @copyright Daniel Pett <dpett@britishmuseum.org>
  * @package Pas
  * @category Pas_View_Helper
  * @uses Zend_Auth Zend Auth
@@ -35,9 +33,9 @@ class Pas_View_Helper_FullName extends Zend_View_Helper_Abstract
      * @return object
      */
     public function getAuth() {
-        $this-&gt;_auth = Zend_Auth::getInstance();
+        $this->_auth = Zend_Auth::getInstance();
 
-        return $this-&gt;_auth;
+        return $this->_auth;
     }
 
     /** The fullname to use and display
@@ -51,12 +49,12 @@ class Pas_View_Helper_FullName extends Zend_View_Helper_Abstract
      * @return string
      */
     public function getFullname() {
-        $user = $this-&gt;getAuth()-&gt;getIdentity();
-        if ($user-&gt;hasIdentity()) {
-            $this-&gt;_fullname = $this-&gt;view-&gt;escape(ucfirst($user-&gt;fullname));
+        $user = $this->getAuth()->getIdentity();
+        if ($user->hasIdentity()) {
+            $this->_fullname = $this->view->escape(ucfirst($user->fullname));
         }
 
-        return $this-&gt;_fullname;
+        return $this->_fullname;
     }
 
     /** The to string function
@@ -64,7 +62,7 @@ class Pas_View_Helper_FullName extends Zend_View_Helper_Abstract
      * @return string
      */
     public function __toString() {
-        return $this-&gt;getFullname();
+        return $this->getFullname();
     }
 
     /** The function to return
@@ -75,4 +73,3 @@ class Pas_View_Helper_FullName extends Zend_View_Helper_Abstract
         return $this;
     }
 }
-</pre></body></html>
