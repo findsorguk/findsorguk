ab0c341a84236d8a93460c78c35c89cae29bea31
Daniel Pett <dpett@britishmuseum.org>
2014-06-25 23:38:19 +0100

    Few model updates

--- app/models/Bibliography.php
+++ app/models/Bibliography.php
@@ -1,38 +1,58 @@
 <?php
-/** A model for manipulating the bibliograpic data
-* Books are stored in publications table
-* @category Pas
-* @package Pas_Db_Table
-* @subpackage Abstract
-* @author Daniel Pett dpett @ britishmuseum.org
-* @copyright 2010 - DEJ Pett
-* @license GNU General Public License
-* @version		1.0
-* @since		22 September 2011
+/** A model for manipulating the bibliograpic data;
+ * Books are stored in publications table
+ *
+ * An example of use:
+ *
+ * <code>
+ * <?php
+ * $model = new Bibliography();
+ * $data = $model->fetchFindBook($id);
+ * ?>
+ * </code>
+ * @author Daniel Pett <dpett@britishmuseum.org>
+ * @copyright (c) 2014, Daniel Pett
+ * @category Pas
+ * @package Pas_Db_Table
+ * @subpackage Abstract
+ * @copyright 2010 - DEJ Pett
+ * @license GNU General Public License
+ * @version 1.0
+ * @since 22 September 2011
+ * @example /app/modules/database/controllers/ReferencesController.php
 */
 
 class Bibliography extends Pas_Db_Table_Abstract {
 
-	protected $_name = 'bibliography';
-	
-	protected $_primary = 'id';
-	
-	/** Get cached data for a book
-	* @param integer $id
-	* @return array
-	*/
-	public function fetchFindBook($id){
-//	if (!$data = $this->_cache->load('bibliobook' . (int)$id)) {
-	$refs = $this->getAdapter();
-	$select = $refs->select()
-		->from($this->_name, array('pages_plates','reference','pubID'))
-		->joinLeft('publications','publications.secuid = bibliography.pubID', 
-		array('publicationtitle' => 'title', 'authors'))
-		->joinLeft('finds','finds.secuid = bibliography.findID', array('id'))
-		->where($this->_name . '.id = ?', $id);
-	$data = $refs->fetchAll($select);
-//	$this->_cache->save($data, 'bibliobook' . (int)$id);
-//	}
-	return $data;
-	}
+    /** The table name
+     * @access protected
+     * @var string
+     */
+    protected $_name = 'bibliography';
+
+    /** The primary key
+     * @access protected
+     * @var int
+     */
+    protected $_primary = 'id';
+
+    /** Get cached data for a book
+     * @access public
+     * @param integer $id
+     * @return array
+     */
+    public function fetchFindBook($id){
+    	if (!$data = $this->_cache->load('bibliobook' . (int)$id)) {
+        $refs = $this->getAdapter();
+        $select = $refs->select()
+                ->from($this->_name, array('pages_plates','reference','pubID'))
+                ->joinLeft('publications','publications.secuid = bibliography.pubID',
+                        array('publicationtitle' => 'title', 'authors'))
+                ->joinLeft('finds','finds.secuid = bibliography.findID', array('id'))
+                ->where($this->_name . '.id = ?', $id);
+        $data = $refs->fetchAll($select);
+    	$this->_cache->save($data, 'bibliobook' . (int)$id);
+    	}
+        return $data;
+    }
 }
--- app/models/Bookmarks.php
+++ app/models/Bookmarks.php
@@ -1,34 +1,50 @@
 <?php
 /** Model for pulling bookmark system data
-* @category   Pas
-* @package    Pas_Db_Table
-* @subpackage Abstract
-* @author Daniel Pett dpett @ britishmuseum.org
-* @copyright 2010 - DEJ Pett
-* @license  GNU General Public License
-* @version  1
-* @since    22 September 2011
-* 
-*/
+ *
+ * An example of use:
+ * <code>
+ * <?php
+ * $model = new Bookmarks();
+ * $data = $model->geValidBookmarks;
+ * ?>
+ * </code>
+ *
+ * @author Daniel Pett <dpett@britishmuseum.org>
+ * @copyright (c) 2014, Daniel Pett
+ * @category   Pas
+ * @package    Db_Table
+ * @license  GNU General Public License
+ * @version  1
+ * @since    22 September 2011
+ * @todo Perhaps deprecate
+ */
 class Bookmarks extends Pas_Db_Table_Abstract {
 
-	protected $_name = 'bookmarks';
-	protected $_primary = 'id';
+    /** The table name
+     * @access protected
+     * @var string
+     */
+    protected $_name = 'bookmarks';
 
-	/** Get all valid bookmarks
-	* @return array
-	* @access public
-	*/
-	public function getValidBookmarks() {
-	if (!$data = $this->_cache->load('bookmarksSite')) {
-	$bookmarks = $this->getAdapter();
-	$select = $bookmarks->select()
-		->from($this->_name, array('image','url','service'))
-		->where('valid = ?',(int)1);
-	$data =  $bookmarks->fetchAll($select);
-	$this->_cache->save($data, 'bookmarksSite');
-	}
-	return $data;
-	}
+    /** The primary key
+     * @access protected
+     * @var type
+     */
+    protected $_primary = 'id';
 
+    /** Get all valid bookmarks
+    * @return array
+    * @access public
+    */
+    public function getValidBookmarks() {
+        if (!$data = $this->_cache->load('bookmarksSite')) {
+            $bookmarks = $this->getAdapter();
+            $select = $bookmarks->select()
+                    ->from($this->_name, array('image','url','service'))
+                    ->where('valid = ?',(int)1);
+            $data =  $bookmarks->fetchAll($select);
+            $this->_cache->save($data, 'bookmarksSite');
+        }
+        return $data;
+    }
 }
