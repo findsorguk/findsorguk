421c183c47223696d66f651833cb047f1e65e184
Daniel Pett <dpett@britishmuseum.org>
2014-06-25 17:31:54 +0100

    Change to ckeditor config
    This fixes #94

--- public_html/js/ckeditor/config.js
+++ public_html/js/ckeditor/config.js
@@ -7,6 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+        config.allowedContent = true;
 	
 	config.toolbar = [
 	                  { name: 'document', items: [ 'Source' ] },
