ff997ea826785525eac6f1713e34364ad64767a8
Daniel Pett <dpett@britishmuseum.org>
2014-06-27 08:01:40 +0100

    Model updates and one deletion

--- app/models/OaiPmhRepositoryTokenTable.php
+++ app/models/OaiPmhRepositoryTokenTable.php
@@ -1,25 +1,54 @@
 <?php
-/** Retrieve and manipulate data for OAI-PMH tokens
-* @category 	Pas
-* @package 		Pas_Db_Table
-* @subpackage 	Abstract
-* @author 		Daniel Pett dpett @ britishmuseum.org
-* @copyright 	2010 - DEJ Pett
-* @license 		GNU General Public License
-* @version 		1
-* @since 		22 September 2011
-*/
-class OaiPmhRepositoryTokenTable
-	extends Pas_Db_Table_Abstract {
-    
-	protected $_primary = 'id';
-	protected $_name = 'oai_pmh_repository_tokens';
-	
-	/** Get a specific token
-	* @param integer $token
-	* @return array
-	* @todo add caching
-	*/
+/**
+ * Retrieve and manipulate data for OAI-PMH tokens
+ *
+ * An example of use:
+ *
+ * <code>
+ * <?php
+ * $tokenTable = new OaiPmhRepositoryTokenTable();
+ * $resumptionToken = $tokenTable->createRow();
+ * $resumptionToken->verb = $verb;
+ * $resumptionToken->metadata_prefix = $metadataPrefix;
+ * $resumptionToken->cursor = $cursor + 30;
+ * $resumptionToken->from = $from;
+ * $resumptionToken->until = $until;
+ * $resumptionToken->set = $set;
+ * $resumptionToken->expiration = self::unixToDb(time() + 60 * 60);
+ * $resumptionToken->useragent = $this->_userAgent();
+ * $resumptionToken->ipaddress = $this->_ipAddress();
+ * $resumptionToken->save();
+ * ?>
+ * </code>
+ *
+ * @author Daniel Pett <dpett@britishmuseum.org>
+ * @copyright (c) 2014 Daniel Pett
+ * @category Pas
+ * @package Db_Table
+ * @subpackage Abstract
+ * @license GNU General Public License
+ * @version 1
+ * @since 22 September 2011
+ * @example /library/Pas/OaiPmhRepository/ResponseGenerator.php
+ */
+class OaiPmhRepositoryTokenTable extends Pas_Db_Table_Abstract {
+
+    /** The table's primary key
+     * @access protected
+     * @var integer
+     */
+    protected $_primary = 'id';
+
+    /** The table's name
+     * @access protected
+     * @var string
+     */
+    protected $_name = 'oai_pmh_repository_tokens';
+
+    /** Get a specific token
+    * @param integer $token
+    * @return array
+    */
     public function getToken($token){
 	$records = $this->getAdapter();
 	$select = $records->select()
