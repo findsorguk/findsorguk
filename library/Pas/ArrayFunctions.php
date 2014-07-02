7ee58a9252ed2522819fd63b7779795dd73c0912
Daniel Pett <dpett@britishmuseum.org>
2014-07-02 22:37:52 +0100

    Remove FCKeditor

--- library/Pas/ArrayFunctions.php
+++ library/Pas/ArrayFunctions.php
@@ -1,16 +1,37 @@
 <?php
+/** A collection of array functions
+ *
+ * An example of use:
+ *
+ * <code>
+ * <?php
+ * $cleaner = new Pas_ArrayFunctions();
+ * $cleanArray = $cleaner->array_cleanup($array);
+ * ?>
+ * </code>
+ *
+ * @author Daniel Pett <dpett@britishmuseum.org>
+ * @copyright (c) 2014 Daniel Pett
+ * @version 1
+ * @license http://URL name
+ * @example path description
+ */
 class Pas_ArrayFunctions {
-	
-	public function array_cleanup( $array ) {
+
+    /** Clean array from getAllParams method
+     * @@access public
+     * @param array $array
+     * @return array
+     */
+    public function array_cleanup( array $array ) {
         $todelete = array('submit','action','controller','module','csrf');
-	foreach( $array as $key => $value ) {
-        foreach($todelete as $match){
-    	if($key == $match){
-    		unset($array[$key]);
-    	}
-        }
+        foreach( $array as $key => $value ) {
+            foreach($todelete as $match){
+                if($key == $match){
+                    unset($array[$key]);
+                }
+            }
         }
         return $array;
-        }
-	
+    }
 }
--- public_html/js/fckeditor/editor/plugins/flickr/lang/da.js
+++
@@ -1,1 +0,0 @@
-﻿FCKLang.btnTitle			= 'Billeder fra Flickr' ;
