c29af0d04534288f6025e3bfe48cbd364d9fc490
Mary Chester-Kadwell <mchesterkadwell@users.noreply.github.com>
2014-08-05 10:58:49 +0100

    New files for Hoards functionality

--- app/models/Hoards.php
+++ app/models/Hoards.php
@@ -1,27 +1,25 @@
 <?php
-/** Model for pulling hoard cover sheets from database
+
+/** Access, manipulate and delete hoards data.
  *
  * An example of use:
  *
  * <code>
  * <?php
- * $model = new Hoards();
- * $data = $model->getHoards();
+ *
  * ?>
  * </code>
  *
- * @author Daniel Pett <dpett@britishmuseum.org>
- * @copyright (c) 2014 Daniel Pett
+ * @author Mary Chester-Kadwell <mchester-kadwell at britishmuseum.org>
+ * @copyright (c) 2014 Mary Chester-Kadwell
  * @category Pas
  * @package Db_Table
  * @subpackage Abstract
- @license http://www.gnu.org/licenses/agpl-3.0.txt GNU Affero GPL v3.0
+ * @license http://www.gnu.org/licenses/agpl-3.0.txt GNU Affero GPL v3.0
  * @version 1
- * @since 22 September 2011
- * @todo add caching
- * @example /app/forms/AdvancedSearchForm.php
+ * @since 4 August 2014
+ * @example /app/modules/database/controllers/HoardsController.php
  */
-
 class Hoards extends Pas_Db_Table_Abstract {
 
     /** The table name
@@ -36,53 +34,39 @@ class Hoards extends Pas_Db_Table_Abstract {
      */
     protected $_primary = 'id';
 
-    /** Retrieval of all hoards on database
-     * @access public
-     * @return array $data
-     * @todo add caching
+    /** The higher level array
+     * @access protected
+     * @var array
      */
-    public function getHoards() {
-        $select = $this->select()
-                ->from($this->_name, array('id', 'term'))
-                ->order('term ASC');
-        return $this->getAdapter()->fetchPairs($select);
-    }
+    protected $_higherlevel = array('admin','flos','fa','hero','treasure');
 
-    /** Retrieval of hoard data by ID number
+    /** The parish stop access
      * @access public
-     * @param integer $id
-     * @return array $data
-     * @todo add caching
+     * @var array
      */
-    public function getHoardDetails($id) {
-        $hoards = $this->getAdapter();
-        $select = $hoards->select()
-                ->from($this->_name)
-                ->joinLeft('periods','periods.id = hoards.period', array('t' => 'term'))
-                ->where('hoards.id =? ',(int)$id);
-        return $hoards->fetchAll($select);
-    }
+    protected $_parishStop = array('admin','flos','fa','hero','treasure','research');
 
-    /** get paginated hoard list
-     * @access public
-     * @param integer $page
-     * @return \Zend_Paginator $paginator
-     * @todo add caching
+    /** The restricted access array
+     * @access protected
+     * @var array
      */
-    public function getHoardList($params) {
-        $hoards = $this->getAdapter();
-        $select = $hoards->select()
-                ->from($this->_name)
-                ->joinLeft('finds','finds.hoardID = hoards.id', array('q' => 'SUM(quantity)'))
-                ->joinLeft('periods','periods.id = hoards.period', array('t' => 'term'))
-                ->group('hoards.id')
-                ->order($this->_name . '.id ASC');
-        $paginator = Zend_Paginator::factory($select);
-        $paginator->setItemCountPerPage(30)->setPageRange(10);
-        if(isset($params['page']) && ($params['page'] != "")) {
-            $paginator->setCurrentPageNumber((int)$params['page']);
-        }
-        return $paginator;
+    protected $_restricted = array(null, 'public','member','research');
+
+    /** The error code thrown by the MySQL database when attempting to enter
+     * a duplicate value into a unique field
+     *
+     */
+    const DUPLICATE_UNIQUE_VALUE_ERROR_CODE = 23000;
+
+    /** Get data for individual hoard record from multiple tables
+     * @access public
+     * @param integer $hoardId
+     * @param string $role
+     * @return array
+     */
+    public function getIndividualHoard($hoardId, $role){
 
     }
+
+
 }
---
+++ app/views/scripts/partials/database/hoardsh2section.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mchester-kadwell
+ * Date: 05/08/14
+ * Time: 10:46
+ */ 
