834bb2972d93ccd9f2703a86a2a6734a3859c049
Mary Chester-Kadwell <mchesterkadwell@users.noreply.github.com>
2014-08-20 17:00:22 +0100

    Files for Archaeology (archaeological context)

---
+++ app/forms/ArchaeologyForm.php
@@ -0,0 +1,38 @@
+<?php
+/** Form for adding and editing archaeological context information for hoards
+ *
+ * An example of use:
+ *
+ * <code>
+ * <?php
+ * $form = new ArchaeologyForm();
+ * $form->setLegend('Also try here');
+ * ?>
+ * </code>
+ *
+ * @author Mary Chester-Kadwell <mchester-kadwell at britishmuseum.org>
+ * @author Daniel Pett <dpett at britishmuseum.org>
+ * @copyright (c) 2014 Mary Chester-Kadwell
+ * @copyright (c) 2014 Daniel Pett
+ * @category Pas
+ * @package Pas_Form
+ * @version 1
+ * @since  20 August 2014
+ * @license http://www.gnu.org/licenses/agpl-3.0.txt GNU Affero GPL v3.0
+ * @example /app/modules/database/controllers/ArchaeologyController.php
+ * @uses Archaeology
+ */
+
+class ArchaeologyForm extends Pas_Form {
+
+    /** The constructor
+     * @access public
+     * @param array $options
+     * @return void
+     *
+     */
+    public function __construct(array $options = null) {
+
+
+    }
+}
---
+++ app/modules/database/controllers/ArchaeologyController.php
@@ -0,0 +1,78 @@
+<?php
+/** Controller for manipulating the archaeological context data
+ *
+ * @author Mary Chester-Kadwell <mchester-kadwell at britishmuseum.org>
+ * @author Daniel Pett <dpett at britishmuseum.org>
+ * @category   Pas
+ * @package Pas_Controller_Action
+ * @subpackage Admin
+ * @copyright  Copyright (c) 2014 Mary Chester-Kadwell
+ * @copyright  Copyright (c) 2011 DEJ Pett dpett @ britishmuseum . org
+ * @license http://www.gnu.org/licenses/agpl-3.0.txt GNU Affero GPL v3.0
+ * @version 1
+ * @uses Finds
+ * @uses Findspots
+ * @uses Archaeology
+ * @uses Publications
+ * @uses HoardForm
+ */
+class Database_ArchaeologyController extends Pas_Controller_Action_Admin {
+
+    /** The archaeological context model
+     * @access protected
+     * @var \Archaeology
+     */
+    protected $_archaeology;
+
+    /** The archaeological context form
+     * @access protected
+     * @var \Archaeology
+     */
+    protected $_archaeologyForm;
+
+    /** Base Url redirect
+     *
+     */
+    const REDIRECT = '/database/hoards/';
+
+    public function getArchaeologyForm() {
+        $this->_archaeologyForm = new ArchaeologyForm();
+        return $this->_archaeologyForm;
+    }
+
+    /** Set up the ACL access and appid from config
+     * @access public
+     * @return void
+     */
+    public function init() {
+        $this->_helper->_acl->deny('public',null);
+        $this->_helper->_acl->allow('member',array('index','add','delete','edit'));
+
+        $this->_archaeology = new Archaeology();
+    }
+
+    /** The index page with no root access
+     * @access public
+     * @return void
+     */
+    public function indexAction() {
+        $this->getFlash()->addMessage('You cannot access the archaeological context index.');
+        $this->_redirect(self::REDIRECT);
+    }
+
+    /** Add a new archaeological context
+     * @return void
+     * @access public
+     * @throws Exception
+     * @throws Pas_Exception_Param
+     */
+    public function addAction() {
+
+            $form = $this->getArchaeologyForm();
+            $form->submit->setLabel('Add archaeological context');
+            $this->view->form = $form;
+
+    }
+
+
+}
