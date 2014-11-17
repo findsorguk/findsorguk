64955939ae84d05b7677ad61968aa59696bcccda
Daniel Pett <dpett@britishmuseum.org>
2014-11-17 16:27:38 +0000

    More html changes

--- app/modules/news/controllers/TheyworkforyouController.php
+++ app/modules/news/controllers/TheyworkforyouController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**  Controller for accessing they work for you based news
  *
  * @category   Pas
@@ -20,51 +21,54 @@
  * @uses PostcodeForm
  *
  *
-*/
-class News_TheyworkforyouController extends Pas_Controller_Action_Admin {
+ */
+class News_TheyworkforyouController extends Pas_Controller_Action_Admin
+{
 
     /** Initialise contexts
      * @access public
      * @return void
      */
-    public function init() {
- 	$this->_helper->_acl->allow(null);
- 	$this->_helper->contextSwitch()->setAutoJsonSerialization(false);
-	$this->_helper->contextSwitch()
-                ->setAutoDisableLayout(true)
-                ->addContext('kml',array('suffix' => 'kml'))
-                ->addContext('rss',array('suffix' => 'rss'))
-                ->addContext('atom',array('suffix' => 'atom'))
-                ->addActionContext('finds', array('xml','json','kml','rss','atom'))
-                ->addActionContext('members',array('xml','json'))
-                ->addActionContext('constituencies',array('xml','json'))
-                ->addActionContext('index',array('xml','json'))
-                ->initContext();
-        
+    public function init()
+    {
+        $this->_helper->_acl->allow(null);
+        $this->_helper->contextSwitch()->setAutoJsonSerialization(false);
+        $this->_helper->contextSwitch()
+            ->setAutoDisableLayout(true)
+            ->addContext('kml', array('suffix' => 'kml'))
+            ->addContext('rss', array('suffix' => 'rss'))
+            ->addContext('atom', array('suffix' => 'atom'))
+            ->addActionContext('finds', array('xml', 'json', 'kml', 'rss', 'atom'))
+            ->addActionContext('members', array('xml', 'json'))
+            ->addActionContext('constituencies', array('xml', 'json'))
+            ->addActionContext('index', array('xml', 'json'))
+            ->initContext();
+
     }
 
     /** Get the index page and results for PAS search of twfy
      * @uses Pas_Twfy_Hansard
      * @return void
      */
-    public function indexAction() {
+    public function indexAction()
+    {
         $term = $this->_getParam('term');
         $search = $term ? $term : 'portable antiquities scheme';
         $twfy = new Pas_Twfy_Hansard();
         $arts = $twfy->get($search, $this->getPage(), 20);
         $data = array();
-        foreach($arts->rows as $row){
+        foreach ($arts->rows as $row) {
             $data[] = get_object_vars($row);
         }
         $pagination = array(
-            'page'          => (int)$this->getPage(),
-            'perpage'      => (int)$arts->info->results_per_page,
+            'page' => (int)$this->getPage(),
+            'perpage' => (int)$arts->info->results_per_page,
             'total_results' => (int)$arts->info->total_results
         );
         $paginator = Zend_Paginator::factory($pagination['total_results']);
         $paginator->setCurrentPageNumber($pagination['page'])
-                ->setItemCountPerPage($pagination['perpage'])
-                ->setCache($this->getCache());
+            ->setItemCountPerPage($pagination['perpage'])
+            ->setCache($this->getCache());
         $this->view->data = $data;
         $this->view->paginator = $paginator;
     }
@@ -75,20 +79,21 @@ class News_TheyworkforyouController extends Pas_Controller_Action_Admin {
      * @throws Pas_Exception_Param
      * @return void
      */
-    public function mpAction() {
-        if($this->_getParam('id',false)) {
-        $person =  new Pas_Twfy_Person();
-        $unclean = $person->get($this->_getParam('id'));
-        $clean = array();
-        foreach($unclean as $object){
-            $mp = array();
-            $object = get_object_vars($object);
-            foreach($object as $k => $v){
-                $mp[$k] = utf8_encode($v);
+    public function mpAction()
+    {
+        if ($this->_getParam('id', false)) {
+            $person = new Pas_Twfy_Person();
+            $unclean = $person->get($this->_getParam('id'));
+            $clean = array();
+            foreach ($unclean as $object) {
+                $mp = array();
+                $object = get_object_vars($object);
+                foreach ($object as $k => $v) {
+                    $mp[$k] = utf8_encode($v);
+                }
+                $clean[] = $mp;
             }
-            $clean[] = $mp;
-        }
-        $this->view->data = $clean;
+            $this->view->data = $clean;
         } else {
             throw new Pas_Exception_Param($this->_missingParameter, 500);
         }
@@ -98,8 +103,9 @@ class News_TheyworkforyouController extends Pas_Controller_Action_Admin {
      * @uses Pas_Twfy_Geometry
      * @throws Pas_Exception_Param
      */
-    public function findsAction(){
-        if($this->_getParam('constituency',false)){
+    public function findsAction()
+    {
+        if ($this->_getParam('constituency', false)) {
             $geo = new Pas_Twfy_Geometry();
             $const = urldecode($this->_getParam('constituency'));
             $cons = $geo->get($const);
@@ -108,18 +114,18 @@ class News_TheyworkforyouController extends Pas_Controller_Action_Admin {
                 $cons->min_lon,
                 $cons->max_lat,
                 $cons->max_lon
-                    );
+            );
             $search = new Pas_Solr_Handler();
             $search->setCore('objects');
             $context = $this->_helper->contextSwitch->getCurrentContext();
             $fields = new Pas_Solr_FieldGeneratorFinds($context);
             $search->setFields($fields->getFields());
             $params = $this->getAllParams();
-            $params['bbox'] = implode(',',$bbox);
+            $params['bbox'] = implode(',', $bbox);
             $search->setFacets(array(
                 'objectType', 'county', 'broadperiod',
                 'institution', 'workflow'
-                ));
+            ));
             $search->setParams($params);
             $search->execute();
             $this->view->facets = $search->processFacets();
@@ -136,19 +142,20 @@ class News_TheyworkforyouController extends Pas_Controller_Action_Admin {
      * @access public
      * @return void
      */
-    public function constituenciesAction() {
+    public function constituenciesAction()
+    {
         $cons = new Pas_Twfy_Constituencies();
         $data = $cons->get('2010-05-07');
         $clean = array();
-        foreach($data as $dat){
-            foreach($dat as $k => $v){
-                $clean[] = array( 'name' => utf8_encode($v));
+        foreach ($data as $dat) {
+            foreach ($dat as $k => $v) {
+                $clean[] = array('name' => utf8_encode($v));
             }
         }
         $paginator = new Zend_Paginator(new Zend_Paginator_Adapter_Array($clean));
         $paginator->setCurrentPageNumber($this->getPage())
-                ->setItemCountPerPage(40)
-                ->setCache($this->getCache());
+            ->setItemCountPerPage(40)
+            ->setCache($this->getCache());
         $this->view->data = $paginator;
     }
 
@@ -157,13 +164,14 @@ class News_TheyworkforyouController extends Pas_Controller_Action_Admin {
      * @uses Zend_Paginator
      * @access public
      */
-    public function membersAction() {
+    public function membersAction()
+    {
         $members = new Pas_Twfy_Mps();
         $data = $members->get();
         $clean = array();
-        foreach($data as $d){
+        foreach ($data as $d) {
             $mp = array();
-            foreach($d as $k => $v){
+            foreach ($d as $k => $v) {
                 $mp[$k] = utf8_encode($v);
             }
             $clean[] = (object)$mp;
@@ -171,8 +179,8 @@ class News_TheyworkforyouController extends Pas_Controller_Action_Admin {
         $paginator = new Zend_Paginator(new Zend_Paginator_Adapter_Array($clean));
         $paginator->setCurrentPageNumber((int)$this->getPage());
         $paginator->setItemCountPerPage(30)
-                ->setPageRange(10)
-                ->setCache($this->getCache());
+            ->setPageRange(10)
+            ->setCache($this->getCache());
         $this->view->data = $paginator;
     }
 
@@ -180,19 +188,20 @@ class News_TheyworkforyouController extends Pas_Controller_Action_Admin {
      * @access public
      * @return void
      */
-    public function findmympAction() {
+    public function findmympAction()
+    {
         $constituency = new Pas_Twfy_FindConstituency();
         $form = new PostcodeForm();
         $form->removeElement('thumbnail');
         $form->removeElement('distance');
         $form->postcode->setLabel('Search by postcode');
         $this->view->form = $form;
-        if($this->getRequest()->isPost() && $form->isValid($this->_request->getPost())){
+        if ($this->getRequest()->isPost() && $form->isValid($this->_request->getPost())) {
             if ($form->isValid($form->getValues())) {
                 $place = $constituency->get($form->getValue('postcode'));
             }
             $this->redirect('/news/theyworkforyou/finds/constituency/' . $place->name);
-        } else  {
+        } else {
             $this->getFlash()->addMessage('Please check and correct errors!');
             $form->populate($form->getValues());
         }
