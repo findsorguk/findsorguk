cad6a2ad8b24d32bb9ebe05f9b303413b9752cca
Daniel Pett <dpett@britishmuseum.org>
2015-01-12 08:12:25 +0000

    Refactor news story controller

--- app/modules/news/controllers/StoryController.php
+++ app/modules/news/controllers/StoryController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**  Controller for all the Scheme's news stories
  *
  * @category   Pas
@@ -7,31 +8,32 @@
  * @copyright  Copyright (c) 2011 DEJ Pett dpett @ britishmuseum . org
  * @license http://www.gnu.org/licenses/agpl-3.0.txt GNU Affero GPL v3.0
  * @version 1
- * @uses News 
+ * @uses News
  * @uses Comments
  * @uses CommentFindForm
-*/
-class News_StoryController extends Pas_Controller_Action_Admin {
+ */
+class News_StoryController extends Pas_Controller_Action_Admin
+{
 
     /** The init controller
      * @access public
      * @return void
      */
-    public function init() {
-        $this->_helper->_acl->allow('public',null);
-        
-        $this->_helper->contextSwitch()->setAutoDisableLayout(true)
-                ->addActionContext('index', array('xml','json'))
-                ->initContext();
+    public function init()
+    {
+        $this->_helper->_acl->allow('public', null);
+        $this->_helper->contextSwitch()->setAutoDisableLayout(true)->addActionContext('index', array('xml', 'json'))
+            ->initContext();
     }
-    
+
     /** For individual article
      * @access public
      * @return void
      * @throws Pas_Exception_Param
      */
-    public function indexAction(){
-        if($this->getParam('id',false)){
+    public function indexAction()
+    {
+        if ($this->getParam('id', false)) {
             $news = new News();
             $this->view->news = $news->getStory($this->getParam('id'));
             $comments = new Comments();
@@ -39,19 +41,23 @@ class News_StoryController extends Pas_Controller_Action_Admin {
             $form = new CommentFindForm();
             $form->submit->setLabel('Add a new comment');
             $this->view->form = $form;
-            if($this->getRequest()->isPost() && $form->isValid($this->_request->getPost())) {
-                $data = $this->_helper->akismet($form->getValues());
-                $data['contentID'] = $this->getParam('id');
-                $data['comment_type'] = 'newsComment';
-                $data['comment_approved'] = 'moderation';
-                $comments->add($data);
-                $this->getFlash()->addMessage('Your comment has been entered and will appear shortly!');
-                $this->redirect('/news/stories/article/id/' . $this->getParam('id'));
-                $this->getFlash()->addMessage('There are problems with your comment submission');
-                $form->populate($form->getValues());
+            if ($this->getRequest()->isPost($this->_request->getPost())) {
+                if ($form->isValid($this->_request->getPost())) {
+                    $data = $this->_helper->akismet($form->getValues());
+                    $data['contentID'] = $this->getParam('id');
+                    $data['comment_type'] = 'newsComment';
+                    $data['comment_approved'] = 'moderation';
+                    $comments->add($data);
+                    $this->getFlash()->addMessage('Your comment has been entered and will appear shortly!');
+                    $this->redirect('/news/story/' . $this->getParam('id'));
+
+                } else {
+                    $this->getFlash()->addMessage('There are problems with your comment submission');
+                    $form->populate($this->_request->getPost());
+                }
             }
-	} else {
+        } else {
             throw new Pas_Exception_Param($this->_missingParameter, 500);
-	}
+        }
     }
 }
