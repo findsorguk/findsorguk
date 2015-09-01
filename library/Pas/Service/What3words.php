0375b5c840d195cd9929632046f94fa3ccc9c1ac
Daniel Pett <dpett@britishmuseum.org>
2015-09-01 16:52:49 +0100

    PhpDoc block and findspot error

--- library/Pas/Service/What3words.php
+++ library/Pas/Service/What3words.php
@@ -1,22 +1,47 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: danielpett
- * Date: 29/08/15
- * Time: 08:04
+/**A service class for shortening and expanding links with goo.gl
+ *
+ * An example of use:
+ *
+ * <code>
+ * <?php
+ * $what3words = new Pas_Service_What3words();
+ * $what3words->setApiKey($key)->setLanguage($lang)->positionToWords(array($x,$y));
+ * ?>
+ * </code>
+ *
+ * @author Daniel Pett <dpett@britishmuseum.org>
+ * @copyright (c) 2015 Daniel Pett
+ * @category   Pas
+ * @package    Pas_Service
+ * @version 1
+ * @license http://www.gnu.org/licenses/agpl-3.0.txt GNU Affero GPL v3.0
+ * @uses Pas_Curl
+ * @example app/models/Findspots.php
  */
 class Pas_Service_What3words
 {
-
+    /** Base url to query */
     const WHAT3WORDS_URI = 'http://api.what3words.com/';
+
+    /** The Api key to use
+     * @access protected
+     * @var string $_apiKey
+     */
     protected $_apiKey;
+
+    /** The default language
+     * @access protected
+     * @var string
+     */
     protected $_language = 'en';
 
-    /**
-     * Convert 3 words or OneWord into position
+    /** Convert 3 words or OneWord into position
      * Takes words either as string, or array of words
      * Returns array of [lat, long]
+     * @access public
+     * @return array
      */
     public function wordsToPosition($words)
     {
@@ -27,13 +52,14 @@ class Pas_Service_What3words
         }
         $data = array('string' => $words);
         return self::requestWords('w3w', $data);
-
     }
 
     /**
      * Convert a position into 3 words
      * Takes position either as string, or array of 2 values
      * Returns array of [word1, word2, word3]
+     * @access public
+     * @return array
      */
     public function positionToWords($position)
     {
@@ -47,6 +73,10 @@ class Pas_Service_What3words
 
     }
 
+    /** Get the data with a specific method using Curl
+     * @access public
+     * @return string
+     */
     public function requestWords($method, $data)
     {
         $data['key'] = $this->getApiKey();
@@ -62,15 +92,17 @@ class Pas_Service_What3words
         }
     }
 
-    /**
+    /** Get the api key
      * @return mixed
+     * @access public
      */
     public function getApiKey()
     {
         return $this->_apiKey;
     }
 
-    /**
+    /** Set the api key
+     * @access public
      * @param mixed $apiKey
      * @return Pas_Service_What3words
      */
@@ -80,15 +112,16 @@ class Pas_Service_What3words
         return $this;
     }
 
-    /**
+    /** Get the language to use
      * @return string
+     * @access public
      */
     public function getLanguage()
     {
         return $this->_language;
     }
 
-    /**
+    /** Set the language
      * @param string $language
      * @return Pas_Service_What3words
      */
