0c76b3ba5d0289a2ccf2dd2094f8190c7ae26d2e
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2018-10-16 15:47:09 +0000

    Check for start and end date exists in the array

--- library/Pas/View/Helper/NomismaRicTypes.php
+++ library/Pas/View/Helper/NomismaRicTypes.php
@@ -108,11 +108,14 @@ class Pas_View_Helper_NomismaRicTypes extends Zend_View_Helper_Abstract
         $html = '';
         $types = array();
         foreach ($data as $rrc) {
+	    $startdate = array_key_exists('startDate', $rrc) ? $rrc->startDate : '';
+            $enddate = array_key_exists('endDate', $rrc) ? $rrc->endDate : '';
+
             $types[] = array(
                 'type' => $rrc->type,
                 'label' => $rrc->label,
-                'startDate' => $rrc->startDate,
-                'endDate' => $rrc->endDate
+                'startDate' => $startdate,
+                'endDate' => $enddate 
             );
         }
         if (!empty($types)) {
