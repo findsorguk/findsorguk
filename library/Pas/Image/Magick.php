809900a05c8a37281ce904b7b7647088f8d71ecb
Liam Rehman <rehmanliam@gmail.com>
2023-04-17 11:09:16 +0100

    refactor: PSR12

--- library/Pas/Image/Magick.php
+++ library/Pas/Image/Magick.php
@@ -1,109 +1,71 @@
 <?php
 /** A class wrapper for the Imagecow library just for PAS use
- * @author Daniel Pett <dpett at britishmuseum.org>
+ *
+ * @author        Daniel Pett <dpett at britishmuseum.org>
  * @copyright (c) 2014 Daniel Pett
- * @category Pas
- * @package Image
- * @since 3/2/12
- * @license http://www.gnu.org/licenses/agpl-3.0.txt GNU Affero GPL v3.0
- * @version 1
- * @uses Imagecow
- * @uses Pas_User_Details
- * @uses Pas_Image_Exception
- * @uses Pas_Image_Rename
+ * @category      Pas
+ * @package       Image
+ * @since         3/2/12
+ * @license       http://www.gnu.org/licenses/agpl-3.0.txt GNU Affero GPL v3.0
+ * @version       1
+ * @uses          Imagecow
+ * @uses          Pas_User_Details
+ * @uses          Pas_Image_Exception
+ * @uses          Pas_Image_Rename
  *
  */
 
 use Imagecow\Image;
 
-
 class Pas_Image_Magick
 {
 
-    /** Set up array of sizes */
-    protected $_sizes;
-
-    /** Create user path string */
-    protected $_userPath;
-
-    protected $_directoryPath;
-
-    /** Create original */
-    protected $_original;
-
-    /** Allowed extensions */
-    protected $_extensions = array('jpg', 'jpeg', 'tiff', 'tif');
-
-    /** Basename of file */
-    protected $_basename;
-
-    /** The record number to create the thumbnail */
-    protected $_imageNumber;
-
-    /** Allowed mime types */
-    protected $_mimeTypes = array(
-        'image/jpeg',
-        'image/pjpeg',
-        'image/tiff',
-        'image/x-tiff'
-    );
-
-    /** User object */
-    protected $_user;
-
     /**  Thumbnail directory */
     const THUMB = '/thumbnails/';
-
     /** Small image directory */
     const SMALL = '/small/';
-
     /** Medium image directory */
     const MEDIUM = '/medium/';
-
     /** Large image directory */
     const LARGE = '/large/';
-
     /** Display image directory */
     const DISPLAY = '/display/';
-
     /** Create directory to store tiffs */
     const TIFFS = '/tiffs/';
-
     /** The default extension */
     const EXT = '.jpg';
-
     /** The permissions for a directory */
     const PERMS = 0777;
-
+    /** Set up array of sizes */
+    protected $_sizes;
+    /** Create user path string */
+    protected $_userPath;
+    protected $_directoryPath;
+    /** Create original */
+    protected $_original;
+    /** Allowed extensions */
+    protected $_extensions = array('jpg', 'jpeg', 'tiff', 'tif');
+    /** Basename of file */
+    protected $_basename;
+    /** The record number to create the thumbnail */
+    protected $_imageNumber;
+    /** Allowed mime types */
+    protected $_mimeTypes
+        = array(
+            'image/jpeg',
+            'image/pjpeg',
+            'image/tiff',
+            'image/x-tiff'
+        );
+    /** User object */
+    protected $_user;
     /** The mime types for tiff images */
     protected $_tiffMimes = array('image/tiff', 'image/x-tiff');
 
-    /** Get the sizes for creation
-     * @access public
-     */
-    public function getSizes()
-    {
-        $this->_sizes = array(
-            array('destination' => self::THUMB, 'width' => 100, 'height' => 100),
-            array('destination' => self::SMALL, 'width' => 40, 'height' => 0),
-            array('destination' => self::MEDIUM, 'width' => 500, 'height' => 0),
-            array('destination' => self::DISPLAY, 'width' => 0, 'height' => 200),
-        );
-        return $this->_sizes;
-    }
-
-    /** Get the user
-     * @access public
-     * @return object
-     */
-    public function getUser()
-    {
-        $this->_user = new Pas_User_Details();
-        return $this->_user;
-    }
-
     /** Set up the image
+     *
      * @access public
+     *
      * @param string $image
      */
     public function setImage($image)
@@ -115,60 +77,57 @@ class Pas_Image_Magick
         return $this;
     }
 
-    /** Set the image id number
+    /** Check permissions for the user directories
+     *
      * @access public
-     * @return int
+     * @return \Pas_Image_Magick
      * @throws Pas_Image_Exception
      */
-    public function setImageNumber($id)
+    public function checkPermissions()
     {
-        if (is_int($id)) {
-            $this->_imageNumber = $id;
-        } else {
-            throw new Pas_Image_Exception('No file to create', 500);
+        //For each directory in the list, check that the directory exists
+        foreach ($this->getSizes() as $dir) {
+            //Set up each directory path
+            $directory = IMAGE_PATH . $this->getUserPath()
+                . $dir['destination'];
+            //Check if directory exists and if not create.
+            if (!is_writable($directory)) {
+                chmod($directory, self::PERMS);
+            } else {
+                throw new Pas_Image_Exception(
+                    'The directory ' . $directory . ' is not writable',
+                    500
+                );
+            }
         }
         return $this;
     }
 
-    /** Get the image number
+    /** Get the sizes for creation
+     *
      * @access public
-     * @return int
      */
-    public function getImageNumber()
+    public function getSizes()
     {
-        return $this->_imageNumber;
-    }
-
-    /** get the image
-     * @access public
-     * @return string Path to file
-     */
-    public function getImage()
-    {
-        //Return the original name
-        return $this->_original;
-    }
-
-    /** Get the array of mime types accepted
-     * @access public
-     * @return array
-     */
-    public function getMimeTypes()
-    {
-        return $this->_mimeTypes;
-    }
-
-    /** Get the basename of the image minus extension
-     * @access public
-     * @return string
-     */
-    public function getBasename()
-    {
-        //Return the basename
-        return $this->_basename;
+        $this->_sizes = array(
+            array(
+                'destination' => self::THUMB,
+                'width'       => 100,
+                'height'      => 100
+            ),
+            array('destination' => self::SMALL, 'width' => 40, 'height' => 0),
+            array('destination' => self::MEDIUM, 'width' => 500, 'height' => 0),
+            array(
+                'destination' => self::DISPLAY,
+                'width'       => 0,
+                'height'      => 200
+            ),
+        );
+        return $this->_sizes;
     }
 
     /** Get the user's path
+     *
      * @access public
      * @return string
      * @throws Pas_Image_Exception
@@ -182,7 +141,9 @@ class Pas_Image_Magick
             //Check if exists
             if (is_null($user)) {
                 //If not throw exception
-                throw new Pas_Image_Exception('No upload directory for that user');
+                throw new Pas_Image_Exception(
+                    'No upload directory for that user'
+                );
             }
             $this->_userPath = '/' . $user->username;
         } else {
@@ -192,6 +153,7 @@ class Pas_Image_Magick
     }
 
     /** Get the directory path
+     *
      * @return string
      * @access public
      */
@@ -201,78 +163,50 @@ class Pas_Image_Magick
     }
 
     /** Set the directory path
+     *
      * @access public
+     *
      * @param string $directoryPath
+     *
      * @throws Zend_Exception
      */
     public function setDirectoryPath($directoryPath)
     {
         if (!is_dir($directoryPath)) {
-            throw new Pas_Image_Exception('That directory path does not exist', 500);
+            throw new Pas_Image_Exception(
+                'That directory path does not exist',
+                500
+            );
         }
         if (!is_writable($directoryPath)) {
-            throw new Pas_Image_Exception('That directory is not writable', 500);
+            throw new Pas_Image_Exception(
+                'That directory is not writable',
+                500
+            );
         }
         $this->_directoryPath = $directoryPath;
         return $this;
     }
 
-
-    /** Check directories exist for a user
+    /** Get the user
+     *
      * @access public
-     * @return \Pas_Image_Magick
+     * @return object
      */
-    public function checkDirectories()
+    public function getUser()
     {
-        //For each directory in the list, check that the directory exists
-        foreach ($this->getSizes() as $dir) {
-            if ($dir['destination'] != self::THUMB) {
-                //Set up each directory path
-                $directory = IMAGE_PATH . $this->getUserPath() . $dir['destination'];
-                //Check if directory exists and if not create.
-                if (!is_dir($directory)) {
-                    $this->_makeDirectory($directory);
-                }
-            } else {
-                $directory = IMAGE_PATH . self::THUMB;
-                if (!is_dir($directory)) {
-                    $this->_makeDirectory($directory);
-                }
-            }
-        }
-        return $this;
-    }
-
-    /** Check permissions for the user directories
-     * @access public
-     * @return \Pas_Image_Magick
-     * @throws Pas_Image_Exception
-     */
-    public function checkPermissions()
-    {
-        //For each directory in the list, check that the directory exists
-        foreach ($this->getSizes() as $dir) {
-            //Set up each directory path
-            $directory = IMAGE_PATH . $this->getUserPath() . $dir['destination'];
-            //Check if directory exists and if not create.
-            if (!is_writable($directory)) {
-                chmod($directory, self::PERMS);
-            } else {
-                throw new Pas_Image_Exception('The directory ' . $directory . ' is not writable', 500);
-            }
-        }
-        return $this;
+        $this->_user = new Pas_User_Details();
+        return $this->_user;
     }
 
     /** Create the different sizes of images
+     *
      * @access public
-     * @throws Pas_Image_Exception
      * @return void
+     * @throws Pas_Image_Exception
      */
-
     public function resize()
     {
-        // Get the image
         $image = $this->getImage();
         // If image parameter not set, throw exception
         if (is_null($image)) {
@@ -283,20 +217,23 @@ class Pas_Image_Magick
         if (!file_exists($image)) {
             throw new Pas_Image_Exception('That image does not exist', 500);
         }
+
         //Make directory check for existence
         $this->checkDirectories();
         // Make directory check for permissions
-//        $this->checkPermissions();
+        // $this->checkPermissions();
 
         //Loop through each size and create the image
         foreach ($this->getSizes() as $resize) {
             // Set the file name
             if ($resize['destination'] == self::THUMB) {
                 // Thumbnail sets record number as thumbnail ID
-                $newImage = IMAGE_PATH . $resize['destination'] . $this->getImageNumber() . self::EXT;
+                $newImage = IMAGE_PATH . $resize['destination']
+                    . $this->getImageNumber() . self::EXT;
             } else {
                 // Normal base name otherwise
-                $newImage = IMAGE_PATH . $this->getUserPath() . $resize['destination'] . $this->getBasename();
+                $newImage = IMAGE_PATH . $this->getUserPath()
+                    . $resize['destination'] . $this->getBasename();
             }
             // Set up the image creation class using imagick
             $surrogate = Image::fromFile($this->getImage(), Image::LIB_IMAGICK);
@@ -316,10 +253,109 @@ class Pas_Image_Magick
         }
     }
 
+    /** get the image
+     *
+     * @access public
+     * @return string Path to file
+     */
+    public function getImage()
+    {
+        //Return the original name
+        return $this->_original;
+    }
+
+    /** Check directories exist for a user
+     *
+     * @access public
+     * @return \Pas_Image_Magick
+     */
+    public function checkDirectories()
+    {
+        //For each directory in the list, check that the directory exists
+        foreach ($this->getSizes() as $dir) {
+            if ($dir['destination'] != self::THUMB) {
+                //Set up each directory path
+                $directory = IMAGE_PATH . $this->getUserPath()
+                    . $dir['destination'];
+                //Check if directory exists and if not create.
+                if (!is_dir($directory)) {
+                    $this->_makeDirectory($directory);
+                }
+            } else {
+                $directory = IMAGE_PATH . self::THUMB;
+                if (!is_dir($directory)) {
+                    $this->_makeDirectory($directory);
+                }
+            }
+        }
+        return $this;
+    }
+
+    /** Make directory if does not exist
+     *
+     * @access public
+     *
+     * @param $path
+     */
+    protected function _makeDirectory($path, $recursive = TRUE)
+    {
+        return mkdir($path, self::PERMS, $recursive);
+    }
+
+    /** Get the image number
+     *
+     * @access public
+     * @return int
+     */
+    public function getImageNumber()
+    {
+        return $this->_imageNumber;
+    }
+
+    /** Set the image id number
+     *
+     * @access public
+     * @return int
+     * @throws Pas_Image_Exception
+     */
+    public function setImageNumber($id)
+    {
+        if (is_int($id)) {
+            $this->_imageNumber = $id;
+        } else {
+            throw new Pas_Image_Exception('No file to create', 500);
+        }
+        return $this;
+    }
+
+    /** Get the basename of the image minus extension
+     *
+     * @access public
+     * @return string
+     */
+    public function getBasename()
+    {
+        //Return the basename
+        return $this->_basename;
+    }
+
+    /** Get the array of mime types accepted
+     *
+     * @access public
+     * @return array
+     */
+    public function getMimeTypes()
+    {
+        return $this->_mimeTypes;
+    }
 
     /** Convert tiff to jpeg
+     *
      * @access public
+     *
      * @param $image
+     *
+     * @throws Pas_Image_Exception
      */
     public function convertTiff($image)
     {
@@ -342,14 +378,5 @@ class Pas_Image_Magick
         $surrogate->save($destination);
         //return to the resize function
         $this->resize();
-    }
-
-    /** Make directory if does not exist
-     * @access public
-     * @param $path
-     */
-    protected function _makeDirectory($path, $recursive = TRUE)
-    {
-        return mkdir($path, self::PERMS, $recursive);
     }
 }
