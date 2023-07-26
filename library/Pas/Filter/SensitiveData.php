b8bc6e90b5133569d8f8edd692cbaf2f23734142
Liam Rehman <rehmanliam@gmail.com>
2023-07-26 16:52:56 +0100

    feat: remove format specific filtering function

--- library/Pas/Filter/SensitiveData.php
+++ library/Pas/Filter/SensitiveData.php
@@ -8,6 +8,22 @@ class Pas_Filter_SensitiveData
 
     protected array $data;
     protected UserPermissions $userPermissions;
+    protected array $finderFields = array('finder', 'finderID');
+    protected array $recorderFields = array(
+        'recorder', 'recorderID', 'createdBy', 'updatedBy', 'username', 'fullname', 'usernameUpdate', 'fullnameUpdate',
+        'creator'
+    );
+    protected array $identifierFields = array(
+        'identifier', 'identifierID', 'secondaryIdentifier', 'identifier1ID',
+        'identifier2ID'
+    );
+    protected array $geoDataFields = array(
+        'lat', 'lon', 'latitude', 'longitude', 'geohash', 'coordinates', 'easting', 'northing'
+    );
+    protected array $fourFigureFields = array('fourFigureLat', 'fourFigureLon');
+    protected array $discoveryMeta = array(
+        'findspotDescription', 'comments', 'landOwnerName', 'address', 'postcode', 'landOwnerID'
+    );
 
     public function __construct()
     {
@@ -30,18 +46,14 @@ class Pas_Filter_SensitiveData
      */
     public function filterSensitivePersonalData(): Pas_Filter_SensitiveData
     {
-        $finderFields = array('finder', 'finderID');
-        $recorderFields = array('recorder', 'recorderID', 'createdBy', 'updatedBy', 'username', 'fullname', 'usernameUpdate', 'fullnameUpdate', 'creator');
-        $identifierFields = array('identifier', 'identifierID', 'secondaryIdentifier', 'identifier1ID', 'identifier2ID');
-
         if (!$this->userPermissions->canRole(UserPermissions::VIEW_RECORD_FINDERS)) {
-            $this->data = $this->filterFieldsFromMultiDimensionalArray($this->data, $finderFields);
+            $this->data = $this->filterFieldsFromMultiDimensionalArray($this->data, $this->finderFields);
         }
         if (!$this->userPermissions->canRole(UserPermissions::VIEW_RECORD_RECORDERS)) {
-            $this->data = $this->filterFieldsFromMultiDimensionalArray($this->data, $recorderFields);
+            $this->data = $this->filterFieldsFromMultiDimensionalArray($this->data, $this->recorderFields);
         }
         if (!$this->userPermissions->canRole(UserPermissions::VIEW_RECORD_IDENTIFIERS)) {
-            $this->data = $this->filterFieldsFromMultiDimensionalArray($this->data, $identifierFields);
+            $this->data = $this->filterFieldsFromMultiDimensionalArray($this->data, $this->identifierFields);
         }
         return $this;
     }
@@ -52,11 +64,6 @@ class Pas_Filter_SensitiveData
      */
     public function filterSensitiveGeoData(): Pas_Filter_SensitiveData
     {
-
-        $geoDataFields = array('lat', 'lon', 'latitude', 'longitude', 'geohash', 'coordinates', 'easting', 'northing');
-        $fourFigureFields = array('fourFigureLat', 'fourFigureLon');
-        $discoveryMeta = array('findspotDescription', 'comments', 'landOwnerName', 'address', 'postcode', 'landOwnerID');
-
         $filteredGeoData = array();
         if (is_array($this->data) && !$this->userPermissions->canRole(UserPermissions::VIEW_GEO_DATA)) {
             foreach ($this->data as $record) {
@@ -64,8 +71,8 @@ class Pas_Filter_SensitiveData
                     $record['gridref'] = $record['fourFigure'];
                 }
 
-                $record = $this->filterFieldsFromData($record, $geoDataFields);
-                $record = $this->filterFieldsFromData($record, $discoveryMeta);
+                $record = $this->filterFieldsFromData($record, $this->geoDataFields);
+                $record = $this->filterFieldsFromData($record, $this->discoveryMeta);
 
                 //If the knownas key exists and is filled in, then it needs restricting
                 if (
@@ -73,7 +80,7 @@ class Pas_Filter_SensitiveData
                     !$this->userPermissions->canRole(UserPermissions::VIEW_KNOWN_AS_GEO_DATA)
                 ) {
                     $record['parish'] = $record['fourFigure'] = $record['gridref'] = 'Restricted Access';
-                    $record = $this->filterFieldsFromData($record, $fourFigureFields);
+                    $record = $this->filterFieldsFromData($record, $this->fourFigureFields);
                 }
                 $filteredGeoData[] = $record;
             }
@@ -88,26 +95,18 @@ class Pas_Filter_SensitiveData
      */
     public function filterExport(?string $format): Pas_Filter_SensitiveData
     {
-        $formatMethod = 'filter' . ucfirst($format) . 'Export';
-        if ($format != null && method_exists($this, $formatMethod)) {
-            $this->$formatMethod();
-        }
-        return $this;
-    }
-
-    /** Filter JSON specific fields from all exports, regardless of permissions
-     * @param $data
-     * @return void
-     */
-    private function filterJsonExport()
-    {
-        $jsonFieldsToRemove = array(
+        $formatsToFilter = array('json', 'geojson', 'kml');
+        $fieldsToFilter = array(
             'address','comments','coordinates','disccircum','easting','elevation','finder','finderID',
-            'findspotDescriptiom','findspotDescription','fullnameUpdate','geohash','gridref','landOwnerID',
-            'landOwnerName','lat','latitude','lon','longitude','northing','postcode','username','usernameUpdate'
+            'findspotDescription','fullnameUpdate','geohash','gridref','landOwnerID','landOwnerName','lat','lon',
+            'northing','postcode','usernameUpdate','finder','fullnameUpdate','latitude','longitude','username',
+            'usernameUpdate','username'
         );
 
-        $this->data = $this->filterFieldsFromMultiDimensionalArray($this->data, $jsonFieldsToRemove);
+        if (in_array(strtolower($format), $formatsToFilter)) {
+            $this->data = $this->filterFieldsFromMultiDimensionalArray($this->data, $fieldsToFilter);
+        }
+        return $this;
     }
 
     protected function filterFieldsFromMultiDimensionalArray(array $data, $filterFields): array
