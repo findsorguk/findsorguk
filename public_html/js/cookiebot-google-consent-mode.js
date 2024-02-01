3719e83ddd6459e309b73ece079dc693cebfb0ff
Liam Rehman <rehmanliam@gmail.com>
2024-02-01 15:06:57 +0000

    feat: update cookiebot script for tcf v2.2

--- public_html/js/cookiebot-google-consent-mode.js
+++ public_html/js/cookiebot-google-consent-mode.js
@@ -6,6 +6,8 @@ function gtag() {
 
 gtag("consent", "default", {
     ad_storage: "denied",
+    ad_personalization: "denied",
+    ad_user_data: "denied",
     analytics_storage: "denied",
     functionality_storage: "denied",
     personalization_storage: "denied",
@@ -13,3 +15,4 @@ gtag("consent", "default", {
     wait_for_update: 2000,
 });
 gtag("set", "ads_data_redaction", true);
+gtag("set", "url_passthrough", true);
