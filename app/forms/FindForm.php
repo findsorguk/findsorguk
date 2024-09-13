e157e2859b0c0db33caad7bb486b79d6d2f84ff9
Liam Rehman <rehmanliam@gmail.com>
2024-09-13 15:41:37 +0100

    feat: allow spaces in treasure case no.

--- app/forms/FindForm.php
+++ app/forms/FindForm.php
@@ -204,7 +204,7 @@ class FindForm extends Pas_Form
         $treasureID->setLabel('Treasure number: ')
             ->setRequired(false)
             ->setAttribs(array('placeholder' => 'T numbers are in the format of YYYYT1234', 'class' => 'span6', 'maxlength' => 15))
-            ->addValidator('Alnum', false, array('allowWhiteSpace' => false))
+            ->addValidator('Alnum', false, array('allowWhiteSpace' => true))
             ->addFilters(array('StripTags', 'StringTrim', 'StringToUpper'));
 
         //Container for hoard
